e856fac8cb82349899ad182fb29e1b197a94db41
stefanstankov <stef.stankov@gmail.com>
2016-10-29 15:16:05 +0300

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -45,6 +45,15 @@
 <!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
 <script src="/assets/js/material-kit.js" type="text/javascript"></script>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <script type="text/javascript">
 $().ready(function() {
   // the body of this function is in assets/material-kit.js
