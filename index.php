c0ae3484b9b6612e7bf502c6ccb31ca9e0996e07
stefanstankov <stef.stankov@gmail.com>
2016-10-29 13:26:52 +0300

    msg

--- index.php
+++ index.php
@@ -29,53 +29,7 @@
 </head>
 
 <body class="index-page">
-	<!-- Navbar -->
-	<nav id="nav-color" class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-				<a class="navbar-brand" href="#"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse navbar-right">
-				<ul class="nav navbar-nav">
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a rel="tooltip" title="Харесайте ни във Facebook" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-								<i class="fa fa-facebook-square"></i>
-							</a>
-						</li>
-						<li>
-							<a rel="tooltip" title="Последвайте ни в Instagram" data-placement="bottom" href="https://www.instagram.com/bglandmarks" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-								<i class="fa fa-instagram"></i>
-							</a>
-						</li>
-						<li>
-							<a rel="tooltip" title="+359 899 001 504" data-toggle="tooltip" data-placement="bottom" href="#" class="btn btn-white btn-simple btn-just-icon">
-								<i class="fa fa-mobile" aria-hidden="true"></i>
-							</a>
-						</li>
-						<li>
-							<a rel="tooltip" title="bglandmarks@gmail.com" data-toggle="tooltip" data-placement="bottom" href="#" class="btn btn-white btn-simple btn-just-icon">
-								<i class="fa fa-envelope-o" aria-hidden="true"></i>
-							</a>
-						</li>
-						<li>
-							<a href="#" data-toggle="modal" data-target="#aboutus">
-								<i id="info" class="material-icons">info_outline</i> За нас
-							</a>
-						</li>
-					</ul>
-			</div>
-			<!--/.nav-collapse -->
-		</div>
-	</nav>
-	<!-- End Navbar -->
-
+	<?php include 'includes/header.php'; ?>
 	<div class="container-fluid">
 		<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
 			<!-- Carousel indicators -->
