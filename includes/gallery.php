606fbe9ec77c446a6cde49dbca0c3bc66d12298e
stefanstankov <stef.stankov@gmail.com>
2016-11-04 16:16:00 +0200

    msg

--- includes/gallery.php
+++ includes/gallery.php
@@ -11,42 +11,6 @@
                     <li data-thumb="../img/thumb/cS-3.jpg">
                         <img src="../img/cS-3.jpg" />
                          </li>
-                    <li data-thumb="../img/thumb/cS-4.jpg">
-                        <img src="../img/cS-4.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-5.jpg">
-                        <img src="../img/cS-5.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-6.jpg">
-                        <img src="../img/cS-6.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-7.jpg">
-                        <img src="../img/cS-7.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-8.jpg">
-                        <img src="../img/cS-8.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-9.jpg">
-                        <img src="../img/cS-9.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-10.jpg">
-                        <img src="../img/cS-10.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-11.jpg">
-                        <img src="../img/cS-11.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-12.jpg">
-                        <img src="../img/cS-12.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-13.jpg">
-                        <img src="../img/cS-13.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-14.jpg">
-                        <img src="../img/cS-14.jpg" />
-                         </li>
-                    <li data-thumb="../img/thumb/cS-15.jpg">
-                        <img src="../img/cS-15.jpg" />
-                         </li>
                 </ul>
             </div>
         </div>
