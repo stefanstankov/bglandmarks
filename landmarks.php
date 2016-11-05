cacba89796a5ce6642c5af5560d626e62aa1113a
stefanstankov <stef.stankov@gmail.com>
2016-11-05 12:48:18 +0200

    msg

--- landmarks.php
+++ landmarks.php
@@ -84,6 +84,65 @@
 							<div class="desc">Регионален исторически музей Кърджали</div>
 						</div>
 					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/orfei.jpg" class="responsive" alt="statue">
+							</a>
+							<div class="desc">Статуята на Орфей</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/perperikon.jpg" class="responsive" alt="perperikon">
+							</a>
+							<div class="desc">Перперикон</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/ustra.jpg" class="responsive" alt="ustra">
+							</a>
+							<div class="desc">Крепостта Устра</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/forest.jpg" class="responsive" alt="stone forest">
+							</a>
+							<div class="desc">Вкаменената гора</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/stone_mushrooms.jpg" class="responsive" alt="stone mushrooms">
+							</a>
+							<div class="desc">Каменните гъби</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/rilski_ezera.jpg" class="responsive" alt="rila">
+							</a>
+							<div class="desc">Рилските езера</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="img">
+							<a href="#">
+								<img src="assets/img/dam_wall.jpg" class="responsive" alt="dam wall">
+							</a>
+							<div class="desc">язовирната стена Кърджали</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
