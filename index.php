3da3de52e3815797863c17310c563f361368b95c
stefanstankov <stef.stankov@gmail.com>
2016-11-07 00:23:05 +0200

    msg

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
               <span>
               <h3>България</h3>
               <span/>
-              <img src="assets/img/main.JPG" class="img-responsive img-circle margin" style="display:inline" alt="RILA">
+              <img src="assets/img/main.JPG" class="img-responsive img-circle margin central-pic" style="display:inline" alt="RILA">
               <span>
               <h3>ДОБРЕ ДОШЛИ</h3>
               <span/>
