bb323e84e800c5a3189bffc75f1aee2bf1803d64
stefanstankov <stef.stankov@gmail.com>
2016-11-06 22:11:23 +0200

    msg

--- index.php
+++ index.php
@@ -9,10 +9,9 @@
 					<div class="col-md-12">
             <div class="container-fluid bg-1 text-center">
   <h3 class="margin">Who Am I?</h3>
-  <img src="assets/img/main.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
+  <img src="assets/img/main.JPG" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
   <h3>I'm an adventurer</h3>
 </div>
-
 						</div>
 					</div>
           <div class="row">
