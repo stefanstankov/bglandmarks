45ab4397b15dd9f46873e3db218263febb2847df
stefanstankov <stef.stankov@gmail.com>
2016-11-06 21:10:35 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -71,22 +71,3 @@ ini_set('display_errors', 1);
     <!--/.nav-collapse -->
   </div>
 </nav>
-<nav class="navbar navbar-default navbar-fixed-top second-nav">
-<div class="container">
-<div class="navbar-header">
-	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-	</button>
-	<a class="navbar-brand" href="http://www.bglandmarks.com"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
-</div>
-<div class="collapse navbar-collapse" id="myNavbar">
-	<ul class="nav navbar-nav navbar-right">
-		<li><a href="landmarks.php">Забележителности</a></li>
-		<li><a href="#">WHAT</a></li>
-		<li><a href="#">Галерия</a></li>
-	</ul>
-</div>
-</div>
-</nav>
