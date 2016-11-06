7fe6c81c592453b7249078d8c4784e059244ec9a
stefanstankov <stef.stankov@gmail.com>
2016-11-06 20:48:56 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -69,24 +69,24 @@ ini_set('display_errors', 1);
         </ul>
     </div>
     <!--/.nav-collapse -->
+		<nav class="navbar navbar-default">
+		<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="http://www.bglandmarks.com"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="landmarks.php">Забележителности</a></li>
+				<li><a href="#">WHAT</a></li>
+				<li><a href="#">Галерия</a></li>
+			</ul>
+		</div>
+		</div>
+		</nav>
   </div>
-</nav>
-<nav id="second-navbar" class="navbar navbar-default navbar-fixed-top">
-<div class="container">
-<div class="navbar-header">
-	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-	</button>
-	<a class="navbar-brand" href="http://www.bglandmarks.com"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
-</div>
-<div class="collapse navbar-collapse" id="myNavbar">
-	<ul class="nav navbar-nav navbar-right">
-		<li><a href="landmarks.php">Забележителности</a></li>
-		<li><a href="#">WHAT</a></li>
-		<li><a href="#">Галерия</a></li>
-	</ul>
-</div>
-</div>
 </nav>
