4b859564d40bbde4bfbdd72c4351ffdd6cfce87e
stefanstankov <stef.stankov@gmail.com>
2016-11-07 02:27:43 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@ ini_set('display_errors', 1);
 
 <head>
 	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="apple-touch-icon" sizes="76x76" href="/assets/img/logo.png">
 	<link rel="icon" type="image/png" href="/assets/img/logo.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
