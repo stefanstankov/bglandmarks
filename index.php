14b877bde6b9e3e28c516f44b7cd13c5a032611e
stefanstankov <stef.stankov@gmail.com>
2016-11-08 01:21:38 +0200

    msg

--- index.php
+++ index.php
@@ -16,13 +16,32 @@
                 </div>
     						</div>
     					</div>
-              <div class="row main-pic">
-                <div class="col-md-12">
-                  <div class="container-fluid text-center">
-                
-                  </div>
-      						</div>
-      					</div>
-
-              </div>
+      <div class="row text-center">
+        <div class="col-sm-4">
+          <div class="thumbnail">
+            <img src="paris.jpg" alt="Paris" width="400" height="300">
+            <p><strong>Paris</strong></p>
+            <p>Friday 27 November 2015</p>
+            <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="thumbnail">
+            <img src="newyork.jpg" alt="New York" width="400" height="300">
+            <p><strong>New York</strong></p>
+            <p>Saturday 28 November 2015</p>
+            <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="thumbnail">
+            <img src="sanfran.jpg" alt="San Francisco" width="400" height="300">
+            <p><strong>San Francisco</strong></p>
+            <p>Sunday 29 November 2015</p>
+            <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 	<?php require 'includes/footer.php' ?>
