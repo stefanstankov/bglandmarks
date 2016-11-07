d012628bf24fa0de82e8175b908a334a2d82a317
stefanstankov <stef.stankov@gmail.com>
2016-11-07 02:43:38 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -31,23 +31,25 @@ ini_set('display_errors', 1);
 
 <body class="index-page">
 	<nav class="navbar navbar-default navbar-fixed-top second-nav">
-	<div class="container">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"aria-expanded="false" aria-controls="navbar">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="http://www.bglandmarks.com"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
-	</div>
-	<div class="collapse navbar-collapse" id="myNavbar">
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="landmarks.php">Забележителности</a></li>
-			<li><a href="#">WHAT</a></li>
-			<li><a href="#">Галерия</a></li>
-		</ul>
-	</div>
-	</div>
+		<div class="container-fluid">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+						<a class="navbar-brand" href="http://www.bglandmarks.com"><i aria-hidden="true" class="fa fa-home"></i>Bulgarian landmarks</a>
+	    </div>
+	    <div id="navbar" class="navbar-collapse collapse navbar-right">
+	        <ul class="nav navbar-nav navbar-right">
+	          <li><a href="landmarks.php">Забележителности</a></li>
+				<li><a href="#">WHAT</a></li>
+				<li><a href="#">Галерия</a></li>
+	        </ul>
+	    </div>
+	    <!--/.nav-collapse -->
+	  </div>
 	</nav>
 <nav id="nav-color" class="navbar navbar-default navbar-fixed-top first-nav">
   <div class="container-fluid">
