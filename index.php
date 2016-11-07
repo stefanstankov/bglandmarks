9daa0bfa5e1b91c3da120071e4fc221ec7860ae2
stefanstankov <stef.stankov@gmail.com>
2016-11-07 22:35:45 +0200

    msg

--- index.php
+++ index.php
@@ -7,21 +7,13 @@
             <div class="row main-pic">
               <div class="col-md-12">
                 <div class="container-fluid text-center">
-                </hr>
-                <br>
-                <br>
+                <hr/>
                 <h3>ДОБРЕ ДОШЛИ</h3>
-                <br>
-                <br>
-                <hr>
+                <hr/>
                   <img src="assets/img/main.JPG" class="img-responsive img-circle margin" style="display:inline" alt="RILA">
-                <hr>
-                <br>
-                <br>
+                <hr/>
                 <h3>В БЪЛГАРИЯ</h3>
-                <br>
-                <br>
-                <hr>
+                <hr/>
                 </div>
     						</div>
     					</div>
