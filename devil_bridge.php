68181980f3c1d3ffbb3d3d6f1d2e62d826211e4c
stefanstankov <stef.stankov@gmail.com>
2016-11-08 00:23:33 +0200

    msg

--- devil_bridge.php
+++ devil_bridge.php
@@ -18,14 +18,14 @@ ini_set('display_errors', 1);
                     <div class="item">
                         <div class="clearfix" style="max-width:474px;">
                             <ul id="image-gallery" class="gallery list-unstyled cS-hidden">
-                                <li data-thumb="../img/thumb/cS-1.jpg">
-                                    <img src="../img/cS-1.jpg" />
+                                <li data-thumb="../img/devil_bridge/thumb/cS-1.jpg">
+                                    <img src="../img/devil_bridge/cS-1.jpg" />
                                      </li>
-                                <li data-thumb="../img/thumb/cS-2.jpg">
-                                    <img src="../img/cS-2.jpg" />
+                                <li data-thumb="../img/devil_bridge/thumb/cS-2.jpg">
+                                    <img src="../img/devil_bridge/cS-2.jpg" />
                                      </li>
-                                <li data-thumb="../img/thumb/cS-3.jpg">
-                                    <img src="../img/cS-3.jpg" />
+                                <li data-thumb="../img/devil_bridge/thumb/cS-3.jpg">
+                                    <img src="../img/devil_bridge/cS-3.jpg" />
                                      </li>
                             </ul>
                         </div>
