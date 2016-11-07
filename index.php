ef07c4f10a1852619181bcddb392d42baecc51f2
stefanstankov <stef.stankov@gmail.com>
2016-11-07 22:17:54 +0200

    msg

--- index.php
+++ index.php
@@ -7,10 +7,24 @@
             <div class="row main-pic">
               <div class="col-md-12">
                 <div class="container-fluid text-center">
+                </hr>
+                <br>
+                <br>
+                <h3>ДОБРЕ ДОШЛИ</h3>
+                <br>
+                <br>
+                </hr>
                   <img src="assets/img/main.JPG" class="img-responsive img-circle margin" style="display:inline" alt="RILA">
+                </hr>
+                <br>
+                <br>
+                <h3>В БЪЛГАРИЯ</h3>
+                <br>
+                <br>
+                </hr>
                 </div>
     						</div>
     					</div>
-        
+
               </div>
 	<?php require 'includes/footer.php' ?>
