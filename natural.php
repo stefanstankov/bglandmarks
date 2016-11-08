335a1f88d9e67d92dbef5a005e0f144a7b52480d
stefanstankov <stef.stankov@gmail.com>
2016-11-08 02:22:03 +0200

    msg

--- natural.php
+++ natural.php
@@ -50,32 +50,34 @@
 	<div class="main main-raised">
 		<div class="section section-basic">
 			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="img">
-							<a href="#">
-								<img src="assets/img/forest.jpg" class="responsive" alt="stone forest">
-							</a>
-							<div class="desc">Вкаменената гора</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="img">
-							<a href="#">
-								<img src="assets/img/stone_mushrooms.jpg" class="responsive" alt="stone mushrooms">
-							</a>
-							<div class="desc">Каменните гъби</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="img">
-							<a href="#">
-								<img src="assets/img/rilski_ezera.jpg" class="responsive" alt="rila">
-							</a>
-							<div class="desc">Рилските езера</div>
-						</div>
-					</div>
-				</div>
+        <div class="row text-center">
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/forest.jpg" alt="Вкаменената гора" width="400" height="300">
+                <p><strong>Вкаменената гора</strong></p>
+                <p>община Ардино/област Кърджали</p>
+                <a id="fr" href="forest.php">
+                <button class="btn" data-toggle="modal" data-target="#fr">Научи повече</button>
+              </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/stone_mushrooms.jpg" alt="Каменните гъби" width="400" height="300">
+                <p><strong>Каменните гъби</strong></p>
+                <p>с. Устрен/община Джебел/област Кърджали</p>
+                <button class="btn" data-toggle="modal" data-target="#myModal">Научи повече</button>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/rilski_ezera.jpg" alt="Рилските езера" width="400" height="300">
+                <p><strong>Рилските езера</strong></p>
+                <p>област Кърджали</p>
+                <button class="btn" data-toggle="modal" data-target="#myModal">Научи повече</button>
+              </div>
+            </div>
+          </div>
 			</div>
 		</div>
 	</div>
