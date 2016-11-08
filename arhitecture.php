bfc2e72237f9d7c4136764c121aca845ae269081
stefanstankov <stef.stankov@gmail.com>
2016-11-08 02:28:16 +0200

    msg

--- arhitecture.php
+++ arhitecture.php
@@ -50,32 +50,38 @@
 	<div class="main main-raised">
 		<div class="section section-basic">
 			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="img">
-							<a href="museum.php">
-								<img src="assets/img/museum.jpg" class="responsive" alt="Museum">
-							</a>
-							<div class="desc">Регионален исторически музей Кърджали</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="img">
-							<a href="#">
-								<img src="assets/img/orfei.jpg" class="responsive" alt="statue">
-							</a>
-							<div class="desc">Статуята на Орфей</div>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="img">
-							<a href="#">
-								<img src="assets/img/dam_wall.jpg" class="responsive" alt="dam wall">
-							</a>
-							<div class="desc">язовирната стена Кърджали</div>
-						</div>
-					</div>
-				</div>
+        <div class="row text-center">
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/museum.jpg" alt="музей" width="400" height="300">
+                <p><strong>Регионален исторически музей Кърджали</strong></p>
+                <p>град Кърджали</p>
+                <a id="ms" href="museum.php">
+                <button class="btn" data-toggle="modal" data-target="#ms">Научи повече</button>
+              </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/orfei.jpg" alt="Статуята на Орфей" width="400" height="300">
+                <p><strong>Статуята на Орфей</strong></p>
+                <p>град Кърджали</p>
+                <a id="st" href="statue.php">
+                <button class="btn" data-toggle="modal" data-target="#st">Научи повече</button>
+              </a>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="thumbnail">
+                <img src="assets/img/dam_wall.jpg" alt="язовирната стена Кърджали" width="400" height="300">
+                <p><strong>язовирна стена</strong></p>
+                <p>град Кърджали</p>
+                <a id="dw" href="dam_wall.php">
+                <button class="btn" data-toggle="modal" data-target="#dw">Научи повече</button>
+              </a>
+              </div>
+            </div>
+          </div>
 			</div>
 		</div>
 	</div>
