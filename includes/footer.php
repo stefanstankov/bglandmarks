7ecfca041416dc140aa6985b464e3fce0d3846c6
stefanstankov <stef.stankov@gmail.com>
2016-11-12 08:44:03 +0200

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -33,6 +33,7 @@
 </body>
 <!--   Core JS Files   -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="/assets/js/material.min.js"></script>
 
@@ -48,6 +49,7 @@
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="../assets/js/lightslider.js"></script>
+ <script src="js/pushy.min.js"></script>
 
 <div id="fb-root"></div>
 <script>
