795f97d52429a69f57f781dcfca7c0f5296282cd
stefanstankov <stef.stankov@gmail.com>
2016-11-13 17:26:57 +0200

    msg

--- devil_bridge.php
+++ devil_bridge.php
@@ -29,7 +29,7 @@ ini_set('display_errors', 1);
 				</div>
 			</div>
 						<hr/>
-            <div class="row d-div">
+            <div class="row">
 					<div class="col-md-4">
             <div class="d-div">
               <p>
