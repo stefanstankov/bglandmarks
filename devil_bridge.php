cc4340c8ab03e295f8273ee14ead35eeb180f119
stefanstankov <stef.stankov@gmail.com>
2016-11-13 16:31:26 +0200

    msg

--- devil_bridge.php
+++ devil_bridge.php
@@ -44,6 +44,9 @@ ini_set('display_errors', 1);
 							<li>
 						<a href="https://bg.wikipedia.org/wiki/%D0%94%D1%8F%D0%B2%D0%BE%D0%BB%D1%81%D0%BA%D0%B8_%D0%BC%D0%BE%D1%81%D1%82_(%D0%9E%D0%B1%D1%89%D0%B8%D0%BD%D0%B0_%D0%90%D1%80%D0%B4%D0%B8%D0%BD%D0%BE)">
 						<i class="fa fa-wikipedia-w" aria-hidden="true"></i>Дяволски мост (Община Ардино)</a></p></li>
+						<li>
+								<a href="https://www.youtube.com/watch?v=8JwftffRd64">
+								<i class="fa fa-youtube" aria-hidden="true"></i>Дяволски мост (видео)</a></p></li>						
 					</ul>
         </div>
 						<div class="row">
