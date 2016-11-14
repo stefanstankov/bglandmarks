96fed6ab2ec7623538b5ab0cfa744edf40195455
stefanstankov <stef.stankov@gmail.com>
2016-11-14 23:23:29 +0200

    msg

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 ?>
         <div class="conteiner-fluid">
             <div class="row main-pic">
-              <div id="bla" class="col-md-12">
-                <div class="container-fluid text-center">
+              <div class="col-md-12">
+                <div id="bla" class="container-fluid text-center">
                 <hr/>
                 <h2>ДОБРЕ ДОШЛИ</h2>
                 <hr/>
