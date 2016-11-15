8c58f114e58876cb7b07ab46b2bc6b3b6250c7d1
stefanstankov <stef.stankov@gmail.com>
2016-11-15 03:08:27 +0200

    msg

--- historical.php
+++ historical.php
@@ -11,7 +11,7 @@
         <div class="row text-center">
           <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="assets/img/bridge.jpg" alt="...">
+      <img src="assets/img/bridge.jpg" alt="Дяволксия мост">
       <div class="caption">
         <h3>Дяволксия мост</h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
@@ -21,7 +21,7 @@
   </div>
     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="assets/img/bridge.jpg" alt="...">
+      <img src="assets/img/assets/img/ustra.jpg" alt="Крепостта Устра">
       <div class="caption">
         <h3>Дяволксия мост</h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
@@ -31,7 +31,7 @@
   </div>
     <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="assets/img/bridge.jpg" alt="...">
+      <img src="assets/img/assets/img/perperikon.jpg" alt="Перперикон">
       <div class="caption">
         <h3>Дяволксия мост</h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
