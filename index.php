0d2932076cbc517e2ba67a56bb98877303609908
stefanstankov <stef.stankov@gmail.com>
2016-11-16 15:52:59 +0200

    msg

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
               </div>
               </div>
             </div>
-            <div id="myCarousel" class="row">
+            <div class="row">
               <div class="col-md-12">
                 <div class="container-fluid">
                   <div  class="carousel slide" data-interval="3000" data-ride="carousel">
