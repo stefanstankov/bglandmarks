bcc2c4edfc30b878dd100f8ea9f45d9e965c3d08
stefanstankov <stef.stankov@gmail.com>
2016-11-17 21:51:34 +0200

    msg

--- arhitecture.php
+++ arhitecture.php
@@ -12,29 +12,24 @@
     <a href="#" class="list-group-item active">
       <h2>Архитектурни</h2>
     </a>
-      <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="assets/img/museum.jpg" alt="Регионален исторически музей Кърджали">
+<div class="col-lg-4 aricle">
+      <img src="assets/img/museum.jpg" alt="Регионален исторически музей Кърджали" width="304" height="236">
       <div class="caption">
         <h3><strong>Регионален исторически музей Кърджали</strong></h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
         <p><a id="db" href="museum.php" class="btn btn-primary" role="button">Научи повече</a></p>
       </div>
     </div>
-  </div>
-    <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="assets/img/orfei.jpg" alt="Статуята на Орфей">
+<div class="col-lg-4 aricle">
+      <img src="assets/img/orfei.jpg" alt="Статуята на Орфей" width="304" height="236">
       <div class="caption">
         <h3><strong>Статуята на Орфей</strong></h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
         <p><a id="db" href="statue.php" class="btn btn-primary" role="button">Научи повече</a></p>
       </div>
     </div>
-  </div>
-    <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="assets/img/dam_wall.jpg" alt="язовирната стена Кърджали">
+<div class="col-lg-4 aricle">
+      <img src="assets/img/dam_wall.jpg" alt="язовирната стена Кърджали" width="304" height="236">
       <div class="caption">
         <h3><strong>язовирната стена Кърджали</strong></h3>
         <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
--- natural.php
+++ natural.php
@@ -13,36 +13,39 @@
   <a href="#" class="list-group-item active">
     <h2>Природни</h2>
   </a>
-    <div class="col-sm-6 col-md-4">
-  <div class="thumbnail">
-    <img src="assets/img/forest.jpg" alt="Вкаменената гора">
+    <div class="col-lg-4 aricle">
+      <br>
+        <br>
+          <br>
+    <img src="assets/img/forest.jpg" alt="Вкаменената гора" width="304" height="236">
     <div class="caption">
       <h3><strong>Вкаменената гора</strong></h3>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
       <p><a id="db" href="forest.php" class="btn btn-primary" role="button">Научи повече</a></p>
     </div>
-  </div>
 </div>
-  <div class="col-sm-6 col-md-4">
-  <div class="thumbnail">
-    <img src="assets/img/stone_mushrooms.jpg" alt="Каменните гъби">
+<div class="col-lg-4 aricle">
+  <br>
+    <br>
+      <br>
+    <img src="assets/img/stone_mushrooms.jpg" alt="Каменните гъби" width="304" height="236">
     <div class="caption">
       <h3><strong>Каменните гъби</strong></h3>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
       <p><a id="db" href="stone_mushrooms.php" class="btn btn-primary" role="button">Научи повече</a></p>
     </div>
   </div>
-</div>
-  <div class="col-sm-6 col-md-4">
-  <div class="thumbnail">
-    <img src="assets/img/rilski_ezera.jpg" alt="Рилските езера">
+<div class="col-lg-4 aricle">
+  <br>
+    <br>
+      <br>
+    <img src="assets/img/rilski_ezera.jpg" alt="Рилските езера" width="304" height="236">
     <div class="caption">
       <h3><strong>Рилските езера</strong></h3>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
       <p><a id="db" href="rila.php" class="btn btn-primary" role="button">Научи повече</a></p>
     </div>
   </div>
-</div>
 </div>
           </div>
 			</div>
