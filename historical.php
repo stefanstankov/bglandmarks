a881ab129041f02106670f2415b71ebc1aded869
stefanstankov <stef.stankov@gmail.com>
2016-11-17 20:09:29 +0200

    msg

--- historical.php
+++ historical.php
@@ -10,9 +10,7 @@
 			<div class="container-fluid">
         <div class="row text-center">
           <div class="panel panel-info">
-      	  <a href="#" class="panel panel-info">
       	    <h2>Исторически</h2>
-      	  </a>
       	    <div class="col-sm-6 col-md-4">
           <div class="thumbnail">
             <img src="assets/img/bridge.jpg" alt="Дяволксия мост">
