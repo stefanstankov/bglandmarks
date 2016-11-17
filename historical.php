099676969a59bc923ea836dc103170919a012286
stefanstankov <stef.stankov@gmail.com>
2016-11-17 21:33:36 +0200

    msg

--- historical.php
+++ historical.php
@@ -11,7 +11,7 @@
         <div class="row text-center">
           <div class="panel panel-info">
             <div class="panel-heading panel-info"><h2>Исторически</h2></div>
-      	    <div class="col-lg-4">
+      	    <div class="col-lg-4 aricle">
             <img src="assets/img/bridge.jpg" alt="Дяволксия мост" width="304" height="236">
             <div class="caption">
               <h3><strong>Дяволксия мост</strong></h3>
@@ -19,7 +19,7 @@
               <p><a id="db" href="devil_bridge.php" class="btn btn-info" role="button">Научи повече</a></p>
             </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 aricle">
             <img src="assets/img/ustra.jpg" alt="Крепостта Устра" width="304" height="236">
             <div class="caption">
               <h3><strong>Крепостта Устра</strong></h3>
@@ -27,15 +27,13 @@
               <p><a id="db" href="ustra.php" class="btn btn-info" role="button">Научи повече</a></p>
             </div>
         </div>
-    	    <div class="col-lg-4">
-          <div class="thumbnail">
+    	    <div class="col-lg-4 aricle">
             <img src="assets/img/perperikon.jpg" alt="Перперикон" width="304" height="236">
             <div class="caption">
               <h3><strong>Перперикон</strong></h3>
               <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
               <p><a id="db" href="perperikon.php" class="btn btn-info" role="button">Научи повече</a></p>
             </div>
-          </div>
         </div>
           </div>
 			</div>
