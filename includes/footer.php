5c233d71c6c3c1af4be44ebfeff25b581e2d6b88
stefanstankov <stef.stankov@gmail.com>
2016-11-19 20:40:03 +0200

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<hr class="featurette-divider">
-      <footer>
+  <footer>
+    <hr class="featurette-divider">
         <p class="pull-right"><a href="#">Back to top</a></p>
         <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
