6ff82911327506de5dbc84a3c00a4ae7080dbddc
stefanstankov <stef.stankov@gmail.com>
2016-11-19 18:48:20 +0200

    msg

--- index.php
+++ index.php
@@ -4,15 +4,5 @@
  		require 'includes/header.php';
 ?>
 <div class="row main-pic">
-            <div class="col-md-12">
-              <div class="container-fluid text-center">
-              <hr/>
-              <h2>ДОБРЕ ДОШЛИ</h2>
-              <hr/>
-                <img src="assets/img/main.JPG" class="img-responsive img-circle margin main-div" alt="язовир Кърджали">
-              <hr/>
-              <h2>В БЪЛГАРИЯ</h2>
-              </div>
-              </div>
-            </div>        
+                 
 	<?php require 'includes/footer.php' ?>
