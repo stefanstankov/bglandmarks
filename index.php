e5c930e34e6a201d7fefb1a4b50269f9360c86cc
stefanstankov <stef.stankov@gmail.com>
2016-11-20 16:33:59 +0200

    msg

--- index.php
+++ index.php
@@ -7,22 +7,34 @@
           <div class="row">
             <div class="col-md-12">
               <img src="assets/img/melnik1.jpg" alt="град Мелник"
+
                 </div>
                 </div>
           <div class="row">
            <div class="col-md-4">
              <h2>Дяволския мост</h2>
-             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-             <p><a class="btn btn-primary" href="#" role="button">Научи повече</a></p>
+             <div class="caption">
+               <h3><strong>Пещера утробата</strong></h3>
+               <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
+               <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
+             </div>
            </div>
            <div class="col-md-4">
              <h2>Вкаменената гора</h2>
-             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+             <div class="caption">
+               <h3><strong>Пещера утробата</strong></h3>
+               <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
+               <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
+             </div>
              <p><a class="btn btn-primary" href="#" role="button">Научи повече</a></p>
           </div>
            <div class="col-md-4">
              <h2>регионален исторически музей, Кърджали</h2>
-             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+             <div class="caption">
+               <h3><strong>Пещера утробата</strong></h3>
+               <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
+               <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
+             </div>
              <p><a class="btn btn-primary" href="#" role="button">Научи повече</a></p>
            </div>
          </div>
