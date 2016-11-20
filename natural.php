4e2221b7852f30755d5367ae00575d8c6d2cccb0
stefanstankov <stef.stankov@gmail.com>
2016-11-20 15:46:35 +0200

    msg

--- natural.php
+++ natural.php
@@ -37,11 +37,22 @@
       <br>
     <img src="assets/img/eagle_rocks.jpg" alt="Орлови скали" width="304" height="236">
     <div class="caption">
-      <h3><strong>Орлови скали</strong></h3>
+      <h3><strong>Тракийско скално светилище Орлови скали</strong></h3>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
       <p><a id="db" href="eagle_rocks.php" class="btn alert-danger" role="button">Научи повече</a></p>
     </div>
   </div>
+  <div class="col-lg-4 aricle">
+    <br>
+      <br>
+        <br>
+      <img src="assets/img/swan_village.jpg" alt="Езерата край село Лебед" width="304" height="236">
+      <div class="caption">
+        <h3><strong>Езерата край село Лебед</strong></h3>
+        <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
+        <p><a id="db" href="eagle_rocks.php" class="btn alert-danger" role="button">Научи повече</a></p>
+      </div>
+    </div>
           </div>
 			</div>
 		</div>
