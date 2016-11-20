d0debcaddd13e20943f2ac7ddd3097293450e6f2
stefanstankov <stef.stankov@gmail.com>
2016-11-20 13:56:49 +0200

    msg

--- devil_bridge.php
+++ devil_bridge.php
@@ -5,12 +5,13 @@ ini_set('display_errors', 1);
 
 	$pageTitle = 'Начало';
 ?>
+<div clas="main-backgroung">
 <?php require 'includes/header.php' ?>
 	<!-- End Navbar -->
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<div class="main main-raised museumpage">
-		<div class="section section-basic db section-size">
+		<div class="section section-basic section-size">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-xs-6">
@@ -53,6 +54,7 @@ ini_set('display_errors', 1);
 		</div>
 	</div>
 	</div>
+		</div>
 	<script>
 	$(function() {
       $('#slider').miniSlider();
