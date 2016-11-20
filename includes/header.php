a40bd96cc03da8d464795ec1ff95769d37eba0d9
stefanstankov <stef.stankov@gmail.com>
2016-11-20 10:45:04 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -32,6 +32,33 @@ ini_set('display_errors', 1);
 </head>
 
 <body class="index-page">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img class="first-slide" src="../assets/img/5.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img class="second-slide" src="../assets/img/4.jpg" alt="Second slide">
+			</div>
+			<div class="item">
+				<img class="third-slide" src="../assets/img/8.jpg" alt="Third slide">
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div><!-- /.carousel -->
 
 	<div class="navbar-wrapper">
 		 <div class="container">
@@ -69,30 +96,3 @@ ini_set('display_errors', 1);
 			 </nav>
 		 </div>
 	 </div>
-	 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-		 <!-- Indicators -->
-		 <ol class="carousel-indicators">
-			 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			 <li data-target="#myCarousel" data-slide-to="1"></li>
-			 <li data-target="#myCarousel" data-slide-to="2"></li>
-		 </ol>
-		 <div class="carousel-inner" role="listbox">
-			 <div class="item active">
-				 <img class="first-slide" src="../assets/img/5.jpg" alt="First slide">
-			 </div>
-			 <div class="item">
-				 <img class="second-slide" src="../assets/img/4.ijpg" alt="Second slide">
-			 </div>
-			 <div class="item">
-				 <img class="third-slide" src="../assets/img/8.jpg" alt="Third slide">
-			 </div>
-		 </div>
-		 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-			 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			 <span class="sr-only">Previous</span>
-		 </a>
-		 <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-			 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			 <span class="sr-only">Next</span>
-		 </a>
-	 </div><!-- /.carousel -->
