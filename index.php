29871e2335d26ebc690424a0c52c8245dc27d48c
stefanstankov <stef.stankov@gmail.com>
2016-11-20 16:37:01 +0200

    msg

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
            <div class="col-md-4">
              <h2>Дяволския мост</h2>
              <div class="caption">
-               <h3><strong>Пещера утробата</strong></h3>
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
              </div>
@@ -22,20 +21,16 @@
            <div class="col-md-4">
              <h2>Вкаменената гора</h2>
              <div class="caption">
-               <h3><strong>Пещера утробата</strong></h3>
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
              </div>
-             <p><a class="btn btn-primary" href="#" role="button">Научи повече</a></p>
           </div>
            <div class="col-md-4">
              <h2>регионален исторически музей, Кърджали</h2>
              <div class="caption">
-               <h3><strong>Пещера утробата</strong></h3>
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
              </div>
-             <p><a class="btn btn-primary" href="#" role="button">Научи повече</a></p>
            </div>
          </div>
     </div>
