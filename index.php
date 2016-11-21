08b28b7f4eceddd62bcde663a13462d9c93c16e8
stefanstankov <stef.stankov@gmail.com>
2016-11-21 10:52:11 +0200

    msg

--- index.php
+++ index.php
@@ -6,21 +6,21 @@
       	<div class="section section-basic text-center article-color ">
           <div class="row">
            <div class="col-md-4 ">
-             <h2>Дяволския мост</h2>
+            <p> <h2>Дяволския мост</h2></p>
              <div class="caption">
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
              </div>
            </div>
            <div class="col-md-4">
-             <h2>Вкаменената гора</h2>
+            <p> <h2>Вкаменената гора</h2></p>
              <div class="caption">
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
              </div>
           </div>
            <div class="col-md-4">
-             <h2>регионален исторически музей, Кърджали</h2>
+            <p> <h2>регионален исторически музей, Кърджали</h2></p>
              <div class="caption">
                <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
                <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
