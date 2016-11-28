92ba0c63bbb81f04a904e17811c6b9508f1c6b15
stefanstankov <stef.stankov@gmail.com>
2016-11-28 18:47:30 +0200

    msg

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
           <h2><?= EDITORS_CHOISE;?></h2>
           <hr/>
           <div class="row">
-            <div class="col-md-8">
-            <div class="flexslider pic-position">
+            <div class="col-md-8 col-md-offset-2">
+            <div class="flexslider">
               <ul class="slides">
                 <li>
                   <img src="/assets/img/main.JPG"/>
