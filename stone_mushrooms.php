f8c5336f9e22dceab9aae4868e82b3ab0c42b941
stefanstankov <stef.stankov@gmail.com>
2016-11-28 16:21:17 +0200

    msg

--- stone_mushrooms.php
+++ stone_mushrooms.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 1);
 
 		<div class="section section-basic text-center article-color container">
 				<div class="row">
-					<h2>Каменните гъби</h2>
+					<h2 class="alert-danger">Каменните гъби</h2>
 			      <hr/>
 					<div class="col-md-6">
 
@@ -36,27 +36,26 @@ ini_set('display_errors', 1);
 				</div>
 						<hr/>
             <div class="row">
-					<div class="col-md-8">
+					<div class="col-md-12">
 						<div class="panel">
 							<div class="panel-heading alert-danger"><h3>Допълнителна информация</h3></div>
  								<p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи. Основния му фонд включва над 27 000 експоната ( 40 000 до днес ) - вещи, снимки и документи, свързани с историята на
                 Кърджалийския край. Експозицията е открита през 1987 година, разположена е на 1800 м2 и е разделена на три секции - отдел „Археология“, отдел „Етнография“ и отдел „Природа“. За всеки един от тях е отделен по един етаж.</p></a>
 	</div>
           </div>
-					<div class="col-md-4">
-						<div class="panel alert-danger">
-	            <div class="panel-heading alert-danger"><h3>Линкове по темата</h3></div>
+          </div>
+
+
+    <ul>
+      <li>
 	  <a href="https://bg.wikipedia.org/wiki/%D0%94%D1%8F%D0%B2%D0%BE%D0%BB%D1%81%D0%BA%D0%B8_%D0%BC%D0%BE%D1%81%D1%82_(%D0%9E%D0%B1%D1%89%D0%B8%D0%BD%D0%B0_%D0%90%D1%80%D0%B4%D0%B8%D0%BD%D0%BE)" class="list-group-item"><i class="fa fa-wikipedia-w" aria-hidden="true"></i>Дяволски мост (Община Ардино)</a>
-	    <a href="https://www.youtube.com/watch?v=8JwftffRd64" class="list-group-item"><i class="fa fa-youtube" aria-hidden="true"></i>Дяволски мост (видео)</a>
-	</div>
-				</div>
-							</div>
+  </li>
+    <li>
+      <a href="https://www.youtube.com/watch?v=8JwftffRd64" class="list-group-item"><i class="fa fa-youtube" aria-hidden="true"></i>Дяволски мост (видео)</a>
+    </li>
+  </ul>
 						<div class="row">
 					<div class="col-md-12">
-
-
-
-
 					<div class="fb-comments" data-width="100%" data-numposts="5"></div>
 				</div>
 			</div>
