bd6a4845427bc8364a59ba5293e58f8a4d041f57
stefanstankov <stef.stankov@gmail.com>
2016-11-29 12:23:06 +0200

    msg

--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,7 @@ ini_set('display_errors', 1);
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
  <link rel="stylesheet" href="assets/css/site.css" />
 	<link rel="stylesheet" href="assets/css/normalize.css" />
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
 
 <body class="index-page">
