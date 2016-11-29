e4448980008b5124af95a969292c91d19f7c0b63
stefanstankov <stef.stankov@gmail.com>
2016-11-29 18:11:05 +0200

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
   <footer>
-        <div>
-    <div class="row container">
+        <div class="container">
+    <div class="row">
       <div class="col-md-4 col-md-offset-1">
         <ul class="footer-ul list-unstyled text-left">
         <li><strong><h2><?= CONTACTS;?></h2></strong></li>
