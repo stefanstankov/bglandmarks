32b3a6a9b3bbd1e7fad98c192923a151d42f346e
stefanstankov <stef.stankov@gmail.com>
2016-11-29 11:43:40 +0200

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -19,10 +19,9 @@
         <div class="text-center center-block">
             <p class="txt-railway">- bglandmarks.com-</p>
             <br />
-            <a href="https://www.facebook.com/bootsnipp"><i class="fa fa-facebook-square fa-3x social"></i></a>
-            <a href="https://twitter.com/bootsnipp"><i class="fa fa-twitter-square fa-3x social"></i></a>
-            <a href="https://plus.google.com/+Bootsnipp-page"><i class="fa fa-google-plus-square fa-3x social"></i></a>
-            <a href="mailto:bootsnipp@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
+            <a href="https://www.instagram.com/bglandmarks"><i class="fa fa-instagram fa-3x social"></i></a>
+            <a href="https://plus.google.com/+Bootsnipp-page"><i class="fa fa-mobile fa-3x social"></i></a>
+            <a href="mailto:bglandmarks@gmail.com"><i class="fa fa-envelope-square fa-3x social"></i></a>
 </div>
       </footer>
       </div>
