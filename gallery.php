49fdd8daff4d3ee55054d9f22e9923227acafad4
stefanstankov <stef.stankov@gmail.com>
2016-11-30 22:36:48 +0200

    msg

--- gallery.php
+++ gallery.php
@@ -7,69 +7,36 @@
 <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <div class="section section-basic text-center article-color container">
-<div id="galleryTab">
-  <a data-rel="all"  href="javascript:;" class="filter active">View all</a>
-  <a data-rel="anim" href="javascript:;" class="filter">Animals</a>
-  <a data-rel="land" href="javascript:;" class="filter">Landscapes</a>
-  <a data-rel="arch" href="javascript:;" class="filter">Architecture</a>
-</div>
-<div class="galleryWrap">
-  <a class="fancybox img-responsive" data-fancybox-group="gallery" data-filter="anim" href="/assets/img/bridge.jpg"><img src="/assets/thumbs/bridge.jpg"  width="304" height="236" /></a>
-  <a class="fancybox img-responsive" data-fancybox-group="gallery" data-filter="anim" href="/assets/img/bridge1.jpg"><img src="/assets/thumbs/bridge1.jpg"  width="304" height="236" /></a>
-  <a class="fancybox img-responsive" data-fancybox-group="gallery" data-filter="land" href="/assets/img/eagle_rocks.jpg"><img src="/assets/thumbs/image/eagle_rocks.jpg" width="304" height="236"/></a>
-  <a class="fancybox img-responsive" data-fancybox-group="gallery" data-filter="land" href="/assets/img/eagle_rocks1.jpg"><img src="/assets/thumbs/image/eagle_rocks1.jpg"  width="304" height="236"/></a>
-  <a class="fancybox img-responsive" data-fancybox-group="gallery" data-filter="arch" href="/assets/img/eagle_rocks2.jpg"><img src="/assets/thumbs/image/eagle_rocks2.jpg" width="304" height="236" /></a>
-</div>
-</div>
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/bridge.jpg" title="Golden Manarola (Sanjeev Deo)">
+    	<img src="/asets/thumbs/img/bridge.jpg" alt="" />
+    </a>
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/bridge1.jpg" title="Codirosso spazzacamino (Massimo Greco _Foligno)">
+    	<img src="/asets/thumbs/img/bridge1.jpg" alt="" />
+    </a>
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks.jpg" title="Morning Twilight (Jose Hamra Images)">
+    	<img src="/asets/thumbs/img/eagle_rocks.jpg" alt="" />
+    </a>
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks1.jpg" title="(Eric Goncalves (cathing up again!))">
+    	<img src="/asets/thumbs/img/eagle_rocks1.jpg" alt="" />
+    </a>
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks2.jpg" title="(Eric Goncalves (cathing up again!))">
+    	<img src="/asets/thumbs/img/eagle_rocks2.jpg" alt="" />
+    </a>
 <script>
-jQuery(function($){
-    // fancybox
-    $(".fancybox").fancybox({
-        modal: true, // disable regular nav and close buttons
-        // add buttons helper (requires buttons helper js and css files)
-        helpers: {
-            buttons: {}
-        }
-    });
-}); // ready
-</script>
-<script>
-jQuery(function ($) {
-    // fancybox
-    $(".fancybox").fancybox({
-        modal: true, // disable regular nav and close buttons
-        // add buttons helper (requires buttons helper js and css files)
-        helpers: {
-            buttons: {}
-        }
-    });
-    // filter selector
-    $(".filter").on("click", function () {
-        var $this = $(this);
-        // if we click the active tab, do nothing
-        if ( !$this.hasClass("active") ) {
-            $(".filter").removeClass("active");
-            $this.addClass("active"); // set the active tab
-            // get the data-rel value from selected tab and set as filter
-            var $filter = $this.data("rel");
-            // if we select view all, return to initial settings and show all
-            $filter == 'all' ?
-                $(".fancybox")
-                .attr("data-fancybox-group", "gallery")
-                .not(":visible")
-                .fadeIn()
-            : // otherwise
-                $(".fancybox")
-                .fadeOut(0)
-                .filter(function () {
-                    // set data-filter value as the data-rel value of selected tab
-                    return $(this).data("filter") == $filter;
-                })
-                // set data-fancybox-group and show filtered elements
-                .attr("data-fancybox-group", $filter)
-                .fadeIn(1000);
-        } // if
-    }); // on
-}); //
+$(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
 </script>
 	<?php require 'includes/footer.php' ?>
