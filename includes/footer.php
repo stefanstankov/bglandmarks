455cc7efefbeaeb35ae67a9c546885dd3862f236
stefanstankov <stef.stankov@gmail.com>
2016-11-30 23:30:09 +0200

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -59,8 +59,8 @@
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <script src="../assets/js/ie-emulation-modes-warning.js"></script>
 <script type="text/javascript" src="/assets/js/jquery-1.10.1.min.js"></script>
-<script type="text/javascript" src="/assets/js/jquery.fancybox.js?v=2.1.5"></script>
-<script type="text/javascript" src="/assets/js/jquery.fancybox-thumbs"></script>
+<script type="text/javascript" src="/assets/js/jquery.fancybox.js"></script>
+<script type="text/javascript" src="/assets/js/jquery.fancybox-thumbs.js"></script>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
