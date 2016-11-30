3577a435bd53158535fc389a6a67acabdb7f5518
stefanstankov <stef.stankov@gmail.com>
2016-11-30 22:50:32 +0200

    msg

--- gallery.php
+++ gallery.php
@@ -3,42 +3,34 @@
  		require 'includes/header.php';
 ?>
   <div class="section section-basic text-center article-color container">
+    <div class="row">
+      <div class="col-md-3">
     <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/bridge.jpg" title="Golden Manarola (Sanjeev Deo)">
     	<img src="/assets/thumbs/img/bridge.jpg" alt="" />
-    </a>
+    </a></div>
+    <div class="col-md-3">
     <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/bridge1.jpg" title="Codirosso spazzacamino (Massimo Greco _Foligno)">
     	<img src="/assets/thumbs/img/bridge1.jpg" alt="" />
-    </a>
+    </a></div>
+    <div class="col-md-3">
     <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks.jpg" title="Morning Twilight (Jose Hamra Images)">
     	<img src="/assets/thumbs/img/eagle_rocks.jpg" alt="" />
-    </a>
+    </a></div>
+    <div class="col-md-3">
     <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks1.jpg" title="(Eric Goncalves (cathing up again!))">
     	<img src="/assets/thumbs/img/eagle_rocks1.jpg" alt="" />
-    </a>
+    </a></div></div>
+      <div class="row">
+            <div class="col-md-3">
     <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks2.jpg" title="(Eric Goncalves (cathing up again!))">
     	<img src="/assets/thumbs/img/eagle_rocks2.jpg" alt="" />
     </a>
+  </div>
+  </div>
   </div>
 <script type="text/javascript">
 	$(document).ready(function() {
 		$(".fancybox").fancybox();
 	});
-</script>
-<script>
-$(document).ready(function() {
-	$(".fancybox-thumb").fancybox({
-		prevEffect	: 'none',
-		nextEffect	: 'none',
-		helpers	: {
-			title	: {
-				type: 'outside'
-			},
-			thumbs	: {
-				width	: 50,
-				height	: 50
-			}
-		}
-	});
-});
 </script>
 	<?php require 'includes/footer.php' ?>
