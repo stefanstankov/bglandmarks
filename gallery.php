4f1ad8e065c84fc8ff77b3a26d234566aed8c23e
stefanstankov <stef.stankov@gmail.com>
2016-11-30 22:38:53 +0200

    msg

--- gallery.php
+++ gallery.php
@@ -7,20 +7,20 @@
 <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <div class="section section-basic text-center article-color container">
-    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/bridge.jpg" title="Golden Manarola (Sanjeev Deo)">
-    	<img src="/asets/thumbs/img/bridge.jpg" alt="" />
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/bridge.jpg" title="Golden Manarola (Sanjeev Deo)">
+    	<img src="/assets/thumbs/img/bridge.jpg" alt="" />
     </a>
-    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/bridge1.jpg" title="Codirosso spazzacamino (Massimo Greco _Foligno)">
-    	<img src="/asets/thumbs/img/bridge1.jpg" alt="" />
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/bridge1.jpg" title="Codirosso spazzacamino (Massimo Greco _Foligno)">
+    	<img src="/assets/thumbs/img/bridge1.jpg" alt="" />
     </a>
-    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks.jpg" title="Morning Twilight (Jose Hamra Images)">
-    	<img src="/asets/thumbs/img/eagle_rocks.jpg" alt="" />
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks.jpg" title="Morning Twilight (Jose Hamra Images)">
+    	<img src="/assets/thumbs/img/eagle_rocks.jpg" alt="" />
     </a>
-    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks1.jpg" title="(Eric Goncalves (cathing up again!))">
-    	<img src="/asets/thumbs/img/eagle_rocks1.jpg" alt="" />
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks1.jpg" title="(Eric Goncalves (cathing up again!))">
+    	<img src="/assets/thumbs/img/eagle_rocks1.jpg" alt="" />
     </a>
-    <a class="fancybox-thumb" rel="fancybox-thumb" href="/asets/img/eagle_rocks2.jpg" title="(Eric Goncalves (cathing up again!))">
-    	<img src="/asets/thumbs/img/eagle_rocks2.jpg" alt="" />
+    <a class="fancybox-thumb" rel="fancybox-thumb" href="/assets/img/eagle_rocks2.jpg" title="(Eric Goncalves (cathing up again!))">
+    	<img src="/assets/thumbs/img/eagle_rocks2.jpg" alt="" />
     </a>
 <script>
 $(document).ready(function() {
