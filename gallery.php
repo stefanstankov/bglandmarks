9710da8a0ffcd50615e06070e4798ac79ca36b84
stefanstankov <stef.stankov@gmail.com>
2016-12-01 08:48:34 +0200

    msg

--- gallery.php
+++ gallery.php
@@ -12,8 +12,46 @@
     <a class="fancybox" href="/assets/img/eagle_rocks1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/eagle_rocks1.jpg" alt="" /></a>
       </p>
     <p>
-    <a class="fancybox" href="/assets/img/eagle_rocks2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/eagle_rocks2.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/dam_wall.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/dam_wall.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/dam_wall1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/dam_wall1.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/dam_wall2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/dam_wall2.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/main.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/main.jpg" alt="" /></a>
   </p>
+  <p>
+  <a class="fancybox" href="/assets/img/main1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/main1.jpg" alt="" /></a>
+  <a class="fancybox" href="/assets/img/main2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/main2.jpg" alt="" /></a>
+  <a class="fancybox" href="/assets/img/main3.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/main3.jpg" alt="" /></a>
+  <a class="fancybox" href="/assets/img/main4.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/main4.jpg" alt="" /></a>
+</p>
+<p>
+<a class="fancybox" href="/assets/img/museum.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/museum.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/museum1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/museum1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/perperikon.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/perperikon.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/perperikon1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/perperikon1.jpg" alt="" /></a>
+</p>
+<p>
+<a class="fancybox" href="/assets/img/orfei.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/orfei.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/statue1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/statue1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/statue2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/statue2.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/swan_village.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/swan_village.jpg" alt="" /></a>
+</p>
+<p>
+<a class="fancybox" href="/assets/img/swan_village1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/swan_village1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/stone_mushrooms.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/stone_mushrooms.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/stone_mushrooms1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/stone_mushrooms1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/stone_mushrooms2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/stone_mushrooms2.jpg" alt="" /></a>
+</p>
+<p>
+<a class="fancybox" href="/assets/img/ustra.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/ustra.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/ustra1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/ustra1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/ustra2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/ustra2.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/utroba_cave.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/utroba_cave.jpg" alt="" /></a>
+</p>
+<p>
+<a class="fancybox" href="/assets/img/utrova_cave1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/utrova_cave1.jpg" alt="" /></a>
+<a class="fancybox" href="/assets/img/utrova_cave2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/utrova_cave2.jpg" alt="" /></a>
+</p>
+
     </div>
 
 	<?php require 'includes/footer.php' ?>
