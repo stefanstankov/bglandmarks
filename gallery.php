74e1b7ae244a98a0acac062414f390ef62a80053
stefanstankov <stef.stankov@gmail.com>
2016-12-01 07:03:22 +0200

    msg

--- gallery.php
+++ gallery.php
@@ -4,10 +4,10 @@
 ?>
   <div class="section section-basic text-center article-color container">
   <p>
-	   <a class="fancybox" href="/assets/img/bridge.jpg" data-fancybox-group="gallery" title=""><img src="/assets/thumbs/img/bridge.jpg" alt="" /></a>
-	   <a class="fancybox" href="/assets/img/bridge1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/thumbs/img/bridge1.jpg" alt="" /></a>
-	  <a class="fancybox" href="/assets/img/eagle_rocks.jpg" data-fancybox-group="gallery" title=""><img src="/assets/thumbs/img/eagle_rocks.jpg" alt="" /></a>
-    <a class="fancybox" href="/assets/img/eagle_rocks1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/thumbs/img/eagle_rocks1.jpg" alt="" /></a>
-    <a class="fancybox" href="/assets/img/eagle_rocks2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/thumbs/img/eagle_rocks2.jpg" alt="" /></a>
+	   <a class="fancybox" href="/assets/img/bridge.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/bridge.jpg" alt="" /></a>
+	   <a class="fancybox" href="/assets/img/bridge1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/bridge1.jpg" alt="" /></a>
+	  <a class="fancybox" href="/assets/img/eagle_rocks.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/eagle_rocks.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/eagle_rocks1.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/eagle_rocks1.jpg" alt="" /></a>
+    <a class="fancybox" href="/assets/img/eagle_rocks2.jpg" data-fancybox-group="gallery" title=""><img src="/assets/img/thumbs/eagle_rocks2.jpg" alt="" /></a>
     </div>
 	<?php require 'includes/footer.php' ?>
