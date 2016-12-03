ef28aade08173a6ce374be6b6ba16e0243d14374
stefanstankov <stef.stankov@gmail.com>
2016-12-03 18:29:48 +0200

    msg

--- natural.php
+++ natural.php
@@ -35,7 +35,7 @@
   </div>
   <div class="row">
   <div class="col-md-4">
-      <img src="assets/img/swan_village.jpg" class="img-responsive" alt="Езерата край село Лебед" width="304" height="236">
+      <img src="assets/img/swan_village.jpg" class="img-responsive" alt=<?= SWAN_VILLAGE;?> width="304" height="236">
       <div class="text-left">
         <h3><strong><?= SWAN_VILLAGE;?></strong></h3>
         <hr>
@@ -43,6 +43,16 @@
         <p><a href="swan_village.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
       </div>
     </div>
+    <div slass="col-md-4">
+      <div class="card">
+  <img class="card-img-top" src="assets/img/swan_village.jpg" alt=<?= SWAN_VILLAGE;?> width="318" height="180">
+  <div class="card-block">
+    <h4 class="card-title"><?= SWAN_VILLAGE;?></h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary"><?= READ_MORE;?></a>
+  </div>
+</div>
+      </div>
           </div>
           </div>
                 <?php require 'includes/footer.php' ?>
