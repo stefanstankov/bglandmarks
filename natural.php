ccd3d68a1d0d2f521f27c5865ddc8f846ecab189
stefanstankov <stef.stankov@gmail.com>
2016-12-03 18:36:42 +0200

    msg

--- natural.php
+++ natural.php
@@ -6,7 +6,7 @@
       <h2><?= NATURAL;?></h2>
         <div class="row">
     <div class="col-md-4">
-    <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt=">Пещера утробата" width="304" height="236">
+    <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt=">Пещера утробата" width="318" height="180">
     <div class="text-left">
       <h3><strong><?= UTROBA;?></strong></h3>
       <hr>
@@ -15,7 +15,7 @@
     </div>
 </div>
 <div class="col-md-4">
-    <img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="Каменните гъби" width="304" height="236">
+    <img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="Каменните гъби" width="318" height="180">
     <div class="text-left">
       <h3><strong><?= STONE;?></strong></h3>
       <hr>
@@ -24,7 +24,7 @@
     </div>
   </div>
 <div class="col-md-4">
-    <img src="assets/img/eagle_rocks.jpg" class="img-responsive" alt="Орлови скали" width="304" height="236">
+    <img src="assets/img/eagle_rocks.jpg" class="img-responsive" alt="Орлови скали" width="318" height="180">
     <div class="text-left">
       <h3><strong><?= EAGLE_ROCKS;?></strong></h3>
       <hr>
@@ -35,7 +35,7 @@
   </div>
   <div class="row">
   <div class="col-md-4">
-      <img src="assets/img/swan_village.jpg" class="img-responsive" alt=<?= SWAN_VILLAGE;?> width="304" height="236">
+      <img src="assets/img/swan_village.jpg" class="img-responsive" alt=<?= SWAN_VILLAGE;?> width="318" height="180">
       <div class="text-left">
         <h3><strong><?= SWAN_VILLAGE;?></strong></h3>
         <hr>
@@ -43,16 +43,6 @@
         <p><a href="swan_village.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
       </div>
     </div>
-    <div slass="col-md-4">
-      <div class="card">
-  <img class="card-img-top" src="assets/img/swan_village.jpg" alt=<?= SWAN_VILLAGE;?> width="318" height="180">
-  <div class="card-block">
-    <h4 class="card-title"><?= SWAN_VILLAGE;?></h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary"><?= READ_MORE;?></a>
-  </div>
-</div>
-      </div>
           </div>
           </div>
                 <?php require 'includes/footer.php' ?>
