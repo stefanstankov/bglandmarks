5d1a70c13b3e51e2d9d2e68f2ac09b8f5abd372a
stefanstankov <stef.stankov@gmail.com>
2016-12-03 18:15:38 +0200

    msg

--- natural.php
+++ natural.php
@@ -9,24 +9,27 @@
     <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt=">Пещера утробата" width="304" height="236">
     <div class="caption">
       <h3><strong><?= UTROBA;?></strong></h3>
+      <hr>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
-      <p><a href="utroba_cave.php" class="btn btn-primary" role="button">Научи повече</a></p>
+      <p><a href="utroba_cave.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
     </div>
 </div>
 <div class="col-md-4">
     <img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="Каменните гъби" width="304" height="236">
     <div class="caption">
       <h3><strong><?= STONE;?></strong></h3>
+      <hr>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
-      <p><a  href="stone_mushrooms.php" class="btn btn-primary" role="button">Научи повече</a></p>
+      <p><a  href="stone_mushrooms.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
     </div>
   </div>
 <div class="col-md-4">
     <img src="assets/img/eagle_rocks.jpg" class="img-responsive" alt="Орлови скали" width="304" height="236">
     <div class="caption">
       <h3><strong><?= EAGLE_ROCKS;?></strong></h3>
+      <hr>
       <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
-      <p><a  href="eagle_rocks.php" class="btn btn-primary" role="button">Научи повече</a></p>
+      <p><a  href="eagle_rocks.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
     </div>
   </div>
   </div>
@@ -35,24 +38,11 @@
       <img src="assets/img/swan_village.jpg" class="img-responsive" alt="Езерата край село Лебед" width="304" height="236">
       <div class="caption">
         <h3><strong><?= SWAN_VILLAGE;?></strong></h3>
-        <p>Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
-        <p><a href="swan_village.php" class="btn btn-primary" role="button">Научи повече</a></p>
-      </div>
-    </div>
-
-<div class="col-md-4">
-<div class="card">
-  <div class="view overlay hm-white-slight">
-    <img src="assets/img/swan_village.jpg" class="img-fluid" alt="">
-  </div>
-    <div class="card-block">
-        <h3><strong><?= SWAN_VILLAGE;?></strong></h3>
         <hr>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Открит е през 1965 година със задачата да проучва, съхранява и популяризира културно-историческото наследство в Източни Родопи...</p>
         <p><a href="swan_village.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-    </div>
-</div>
       </div>
+    </div>
           </div>
           </div>
                 <?php require 'includes/footer.php' ?>
