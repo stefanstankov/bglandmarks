b2df9455097aebb5583addb437b60c030a91a609
stefanstankov <stef.stankov@gmail.com>
2016-12-04 17:09:45 +0200

    msg

--- en.php
+++ en.php
@@ -27,5 +27,10 @@ define('STONE', 'The Stone Mushrooms');
 define('USTRA', 'Ustra Medieval Fortress');
 define('UTROBA', 'Utroba Cave (The Womb Cave)');
 define('READ_MORE', 'Read more');
+define('UTROBA_ARTICLE', 'Location - it is situated in the rocky area of Tangradak Kaya, close to to Nenkovo village, 20 kilometres away from Kardzhali.
+Accessibility of site - To reach the cave, a visitors should make a return from the road Kardzhali - Haskovo towards Padartsi and Dazhdovnitsa villages. Following a scenic
+route along Kardzhali Dam, a kilometre after Borovitsa cotage, a gravel turn is reached. A signposted trail, inaccessible for vehicles, begins from there. The trail is wide but very steep. There are three big gazebos located along the it, several benches, as well as a shelter and fireplace beneath the summit.
+Public transportation - There is a regular bus line from Kardzhali to Nenkovo village.
+Description of site - It is artificial cave. The locals call this Tangradak Kaya (stone). The name itself comes from the impresive acoustic echo inside the cave. The entire cave is literally shaped like female genitalia and fully matches the description of womb - caves. The length of the cave is 22 metres. The first 16 metres of the cave are formed of natural carst. It is a natural split in the rocks. The last 6 metres were hollowed out and the shape adapted to become an anatomical female symbol. The height of the entrance is 3 metres and the width is 2,5 meters so there is enough light coming from the outside. At the north end of the cave, there is a man-made aftar which symbolizes the womb. Water drips from the cave and collects in a small pool at the base. The water never dries out. In the Thracian religion (Orphism), cult locations were always at the peak of a mountain housing a cave with drizzling water. Therefore, for the Thracians, this cave was a symbol of the womb, conception and rebirth.');
 
 ?>
