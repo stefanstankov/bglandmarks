475262a71ed9547b32c6e44e59dd771fda8760b1
stefanstankov <stef.stankov@gmail.com>
2016-12-08 00:35:59 +0200

    msg

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
       </div>
       </div>
       </div>
+      <hr/>
+        <h2><?= LOCATION;?></h2>
+        <hr/>
       <div class="row">
     <div class="col-md-12 custom_jumbo">
       <div id="map"></div>
