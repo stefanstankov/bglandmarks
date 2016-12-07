7a966737acb2cc075660f8570a1fc3924a6058ff
stefanstankov <stef.stankov@gmail.com>
2016-12-07 20:50:39 +0200

    msg

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
         <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
       </div>
-      <div>
       </div>
       </div>
+        </div>
           <h2><?= EDITORS_CHOISE;?></h2>
           <hr/>
           <div class="row">
