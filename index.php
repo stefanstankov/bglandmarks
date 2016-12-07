2f050beedcd244927f9eee26d0c21881fa6816b4
stefanstankov <stef.stankov@gmail.com>
2016-12-08 00:40:50 +0200

    msg

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
   </div>
   </div>
           <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12>
             <div class="flexslider">
               <ul class="slides">
                 <li>
