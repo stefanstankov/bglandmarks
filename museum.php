aefc492bdc7cad9ebc25fa4c792d5fb450134d61
stefanstankov <stef.stankov@gmail.com>
2016-12-09 01:43:08 +0200

    msg

--- museum.php
+++ museum.php
@@ -51,6 +51,7 @@ ini_set('display_errors', 1);
 				<ol class="breadcrumb">
 			<li><strong>Линкове по темата</strong></li>
 			<li><a href="https://bg.wikipedia.org/wiki/%D0%A0%D0%B5%D0%B3%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D0%B5%D0%BD_%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8_%D0%BC%D1%83%D0%B7%D0%B5%D0%B9,_%D0%9A%D1%8A%D1%80%D0%B4%D0%B6%D0%B0%D0%BB%D0%B8"><i class="fa fa-wikipedia-w" aria-hidden="true"></i>Регионален исторически музей Кърджали</a></li>
+						<li><a href="http://www.kardjali.bg/en/?pid=6,12 "><i class="fa fa-wikipedia-w" aria-hidden="true"></i>Регионален исторически музей Кърджали</a></li>
 			<li> <a href="http://www.rim-kardzhali.bg/">Официален сайт</a></li>
 		</ol>
 			</div>
