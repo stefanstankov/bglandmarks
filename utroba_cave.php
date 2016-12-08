e3e12f16c349b2dc0fd76a02a38d495d56df4c95
stefanstankov <stef.stankov@gmail.com>
2016-12-09 00:39:03 +0200

    msg

--- utroba_cave.php
+++ utroba_cave.php
@@ -39,9 +39,14 @@ ini_set('display_errors', 1);
 				</div>
 				</div>
             <div class="row">
-					<div class="col-md-12">
+					<div class="col-md-6">
 						<div id="map"></div>
           </div>
+					<div class="col-md-6">
+					<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ucQ0k-_69pY"></iframe>
+</div>
+      </div>
 							</div>
 						<div class="row">
 					<div class="col-md-12">
