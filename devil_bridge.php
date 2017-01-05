3f99c940c3f5c84d0215e4486a80bcbced345618
stefanstankov <stef.stankov@gmail.com>
2017-01-05 10:34:21 +0200

    msg

--- devil_bridge.php
+++ devil_bridge.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 1);
 
 		<div class="section section-basic text-center article-color container">
 				<div class="row">
-					<h2><?= BRIDGE;?>"></h2>
+					<h2><?= BRIDGE;?></h2>
 			      <hr/>
 					<div class="col-md-8 col-md-offset-2">
 
