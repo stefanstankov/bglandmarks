76cf95238a2e98d92a075916770a06f4b3c382ca
stefanstankov <stef.stankov@gmail.com>
2017-01-05 11:28:03 +0200

    msg

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 ?>
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-      	<div class="section section-basic text-center article-color container">
+      	<div class="section section-basic text-center article-color article-color1 container">
           <div class="row">
                <div class="col-md-12 custom_jumbo">
           <div class="jumbotron">
