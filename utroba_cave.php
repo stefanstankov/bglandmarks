2d1ecdadbba4336afc22ab852673214b4ce3289c
stefanstankov <stef.stankov@gmail.com>
2017-01-08 13:13:45 +0200

    adding weather

--- utroba_cave.php
+++ utroba_cave.php
@@ -54,9 +54,9 @@ ini_set('display_errors', 1);
 						<div class="row">
 					<div class="col-md-12">
 					<div class="fb-comments" data-width="100%" data-numposts="5"></div>
+						<div class="fb-share-button pull-left" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
 				</div>
 			</div>
-			<div class="fb-share-button" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
 			<div class="row">
 				<div class="col-md-12">
 				<h2><?= FROM_CATEGORY;?></h2>
