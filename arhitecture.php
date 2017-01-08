05932386aec29ffcae321b348390fb7a9d0b3c4c
stefanstankov <stef.stankov@gmail.com>
2017-01-08 14:00:12 +0200

    adding weather

--- arhitecture.php
+++ arhitecture.php
@@ -2,13 +2,13 @@
   	$pageTitle = 'Начало';
 ?>
 <?php	require 'includes/header.php';?>
-		<div class="section section-basic text-center article-color section-custom container">
+		<div class="section section-basic text-center article-color container">
       <h2><?= ARCHITECTURAL;?></h2>
         <div class="row">
 <div class="col-md-4">
   <a class="link_color" href="museum.php">
   <div class="card">
-      <img src="assets/img/museum.jpg" class="card-img-top" alt="Регионален исторически музей Кърджали" width="318" height="180">
+      <img src="assets/img/museum.jpg" class="img-responsive" alt="Регионален исторически музей Кърджали" width="318" height="180">
       <div class="card-block">
         <h3 class="card-title"><strong><?= MUSEUM;?></strong></h3>
         <p class="text-center"><a href="museum.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
@@ -19,7 +19,7 @@
 <div class="col-md-4">
   <a class="link_color" href="statue.php">
   <div class="card">
-      <img src="assets/img/orfei.jpg" class="card-img-top" alt="Статуята на Орфей" width="318" height="180">
+      <img src="assets/img/orfei.jpg" class="img-responsive" alt="Статуята на Орфей" width="318" height="180">
       <div class="card-block">
         <h3 class="card-title"><strong><?= STATUE;?></strong></h3>
         <p class="text-center"><a href="statue.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
@@ -30,7 +30,7 @@
 <div class="col-md-4">
   <a class="link_color" href="dam_wall.php">
   <div class="card">
-      <img src="assets/img/dam_wall.jpg" class="card-img-top" alt="язовирната стена Кърджали" width="318" height="180">
+      <img src="assets/img/dam_wall.jpg" class="img-responsive" alt="язовирната стена Кърджали" width="318" height="180">
       <div class="card-block">
         <h3 class="card-title"><strong><?= Kardzhali_Dam;?></strong></h3>
         <p class="text-center"><a href="dam_wall.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
@@ -43,7 +43,7 @@
   <div class="col-md-4">
     <a class="link_color" href="devil_bridge.php">
     <div class="card">
-  <img src="assets/img/bridge.jpg" class="card-img-top" alt="Дяволксия мост" width="318" height="180">
+  <img src="assets/img/bridge.jpg" class="img-responsive" alt="Дяволксия мост" width="318" height="180">
   <div class="card-block">
     <h3 class="card-title"><strong><?= BRIDGE;?></strong></h3>
     <p class="text-center"><a href="devil_bridge.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
--- historical.php
+++ historical.php
@@ -2,13 +2,13 @@
   	$pageTitle = 'Начало';
 ?>
    	<?php	require 'includes/header.php';?>
-		<div class="section section-basic text-center article-color section-custom container">
+		<div class="section section-basic text-center article-color container">
           <h2><?= HISTORICAL;?></h2>
               <div class="row">
         <div class="col-md-4">
           <a  class="link_color" href="ustra.php">
             <div class="card">
-            <img src="assets/img/ustra.jpg" class="card-img-top" alt="Крепостта Устра" width="318" height="180">
+            <img src="assets/img/ustra.jpg" class="img-responsive" alt="Крепостта Устра" width="318" height="180">
             <div class="card-block">
               <h3 class="card-title"><strong><?= USTRA;?></strong></h3>
               <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
@@ -19,7 +19,7 @@
     	    <div class="col-md-4">
             <a class="link_color" href="perperikon.php">
               <div class="card">
-            <img src="assets/img/perperikon.jpg" class="card-img-top" alt="Перперикон" width="318" height="180">
+            <img src="assets/img/perperikon.jpg" class="img-responsive" alt="Перперикон" width="318" height="180">
             <div class="card-block">
               <h3 class="card-title"><strong><?= PERPERIKON;?></strong></h3>
               <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
