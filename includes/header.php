c52485512f1baa0c4b6197a45581a2e8c3332773
stefanstankov <stef.stankov@gmail.com>
2017-01-08 13:52:36 +0200

    adding weather

--- includes/header.php
+++ includes/header.php
@@ -33,11 +33,12 @@ ini_set('display_errors', 1);
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 	<!-- CSS Files -->
 	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
-	<link href="/assets/css/style.css" rel="stylesheet" />
+
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
- <link rel="stylesheet" href="assets/css/site.css" />
 	<link rel="stylesheet" href="assets/css/normalize.css" />
+  <link rel="stylesheet" href="assets/css/site.css" />
+	<link href="/assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body class="index-page">
