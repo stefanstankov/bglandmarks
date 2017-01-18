9ae48794fb3add1f4be747451bd8ab13bdfd52ae
stefanstankov <stef.stankov@gmail.com>
2017-01-18 12:17:31 +0200

    adding togle

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         <div id="demo" class="collapse">
         <?= ABOUT_BG;?>
         </div>
-        <button type="button" class="btn btn-primary" role="button" data-toggle="collapse" data-target="#demo">
+          <p class="text-center"><a href="#" class="btn btn-primary" type="button" data-toggle="collapse" data-target="#demo">
       <span class="glyphicon glyphicon-collapse-down"></span> Open
-    </button>
+      </a></p>
       </div>
       </div>
       </div>
