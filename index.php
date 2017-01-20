ea597098456e8aaab000704b8760d74029b046df
stefanstankov <stef.stankov@gmail.com>
2017-01-20 15:57:49 +0200

    msg

--- index.php
+++ index.php
@@ -112,6 +112,8 @@
       var uluru = {lat: 42.697626, lng: 23.322284};
       var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
+        scrl
+        scrollwheel:false;
         center: uluru
       });
       var marker = new google.maps.Marker({
