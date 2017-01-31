01c069cdff8393635f4811145881c2d39a2fb20e
stefanstankov <stef.stankov@gmail.com>
2017-01-31 17:35:18 +0200

    test

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
                   <h2><?= EDITORS_CHOISE;?></h2>
                   <hr/>
           <div class="row">
-           <div class="col-md-4 ">
+           <div class="col-md-3">
              <a class="link_color" href="utroba_cave.php">
              <div class="card">
            <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt=">Пещера утробата">
@@ -88,7 +88,7 @@
            </div>
          </a>
           </div>
-           <div class="col-md-4">
+           <div class="col-md-3">
              <a class="link_color" href="dam_wall.php">
              <div class="card">
                  <img src="assets/img/dam_wall.jpg" class="img-responsive" alt="язовирната стена Кърджали" >
@@ -99,13 +99,24 @@
                    </div>
                    </a>
           </div>
-           <div class="col-md-4">
+           <div class="col-md-3">
              <a  class="link_color" href="ustra.php">
                <div class="card">
                <img src="assets/img/ustra.jpg" class="img-responsive" alt="Крепостта Устра">
                <div class="card-block">
                  <h3 class="card-title"><strong><?= USTRA;?></strong></h3>
                  <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
+               </div>
+                 </div>
+                 </a>
+           </div>
+           <div class="col-md-3">
+             <a  class="link_color" href="stone_mushrooms.php">
+               <div class="card">
+               <img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="Каменните гъби">
+               <div class="card-block">
+                 <h3 class="card-title"><strong><?= STONE;?></strong></h3>
+                 <p class="text-center"><a href="stone_mushrooms.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
                </div>
                  </div>
                  </a>
