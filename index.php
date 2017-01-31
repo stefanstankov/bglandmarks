ca73f4c51b6ee946d65051bb8277fb208a479f0b
stefanstankov <stef.stankov@gmail.com>
2017-01-31 10:03:42 +0200

    test

--- index.php
+++ index.php
@@ -20,6 +20,11 @@
       </div>
       </div>
       </div>
+      <div class="row">
+        <div class="col-md-12">
+          <!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/43846cc373dc505d9b4883eef7963733.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+        </div>
+        </div>
           <div class="row">
             <div class="col-md-6">
               <h2><?= RANDOM_PICS;?></h2>
