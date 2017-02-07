77115961ff17be8d709844144ac4614ce737e382
stefanstankov <stef.stankov@gmail.com>
2017-02-07 22:15:11 +0200

    msg

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                <div class="col-md-12 custom_jumbo">
           <div class="jumbotron">
 
-            <div class="zoomIn"><h1 class="custom-div"><?= WELCOME;?></h1></div>
+            <h1 class="custom-div tada"><?= WELCOME;?></h1>
             <a href="#" type="button" class="btn btn-custom" data-toggle="collapse" data-target="#demo">
           <span class="glyphicon glyphicon-info-sign"></span>
      </a>
