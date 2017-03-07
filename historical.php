cf49b1f0dd2e91ef8a9245d6fb3c68ecf175346a
stefanstankov <stef.stankov@gmail.com>
2017-03-07 09:27:13 +0200

    msg

--- historical.php
+++ historical.php
@@ -7,23 +7,19 @@
               <div class="row">
         <div class="col-md-4">
           <a  class="link_color" href="ustra.php">
-            <div class="card">
             <img src="assets/img/ustra.jpg" class="img-responsive" alt="Крепостта Устра">
             <div class="card-block">
               <h3 class="card-title"><strong><?= USTRA;?></strong></h3>
               <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
             </div>
-              </div>
               </a>
         </div>
     	    <div class="col-md-4">
             <a class="link_color" href="perperikon.php">
-              <div class="card">
             <img src="assets/img/perperikon.jpg" class="img-responsive" alt="Перперикон">
             <div class="card-block">
               <h3 class="card-title"><strong><?= PERPERIKON;?></strong></h3>
               <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-            </div>
               </div>
               </a>
         </div>
