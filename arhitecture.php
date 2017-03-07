c54342c431a0dd74270578c7e51f1531d556aae2
stefanstankov <stef.stankov@gmail.com>
2017-03-07 09:32:42 +0200

    msg

--- arhitecture.php
+++ arhitecture.php
@@ -7,34 +7,28 @@
         <div class="row">
 <div class="col-md-4">
   <a class="link_color" href="museum.php">
-  <div class="card">
       <img src="assets/img/museum.jpg" class="img-responsive" alt="Регионален исторически музей Кърджали">
       <div class="card-block">
         <h3 class="card-title"><strong><?= MUSEUM;?></strong></h3>
         <p class="text-center"><a href="museum.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </div>
         </div>
         </a>
     </div>
 <div class="col-md-4">
   <a class="link_color" href="statue.php">
-  <div class="card">
       <img src="assets/img/orfei.jpg" class="img-responsive" alt="Статуята на Орфей">
       <div class="card-block">
         <h3 class="card-title"><strong><?= STATUE;?></strong></h3>
         <p class="text-center"><a href="statue.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </div>
         </div>
         </a>
     </div>
 <div class="col-md-4">
   <a class="link_color" href="dam_wall.php">
-  <div class="card">
       <img src="assets/img/dam_wall.jpg" class="img-responsive" alt="язовирната стена Кърджали">
       <div class="card-block">
         <h3 class="card-title"><strong><?= Kardzhali_Dam;?></strong></h3>
         <p class="text-center"><a href="dam_wall.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </div>
         </div>
         </a>
     </div>
@@ -42,12 +36,10 @@
   <div class="row">
   <div class="col-md-4">
     <a class="link_color" href="devil_bridge.php">
-    <div class="card">
   <img src="assets/img/bridge.jpg" class="img-responsive" alt="Дяволксия мост" width="318" height="180">
   <div class="card-block">
     <h3 class="card-title"><strong><?= BRIDGE;?></strong></h3>
     <p class="text-center"><a href="devil_bridge.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-  </div>
     </div>
     </a>
     </div>
--- natural.php
+++ natural.php
@@ -7,34 +7,28 @@
         <div class="row">
     <div class="col-md-4">
       <a class="link_color" href="utroba_cave.php">
-      <div class="card">
     <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt=">Пещера утробата">
     <div class="card-block">
       <h3 class="card-title"><strong><?= UTROBA;?></strong></h3>
       <p class="text-center"><a href="utroba_cave.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </div>
     </div>
   </a>
 </div>
 <div class="col-md-4">
   <a class="link_color" href="stone_mushrooms.php">
-  <div class=" card">
     <img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="Каменните гъби">
     <div class="card-block">
       <h3 class="card-title"><strong><?= STONE;?></strong></h3>
       <p class="text-center"><a  href="stone_mushrooms.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
     </div>
-    </div>
   </a>
   </div>
 <div class="col-md-4">
   <a class="link_color" href="eagle_rocks.php">
-  <div class="card">
     <img src="assets/img/eagle_rocks.jpg" class="img-responsive" alt="Орлови скали">
     <div class="card-block">
       <h3 class="card-title"><strong><?= EAGLE_ROCKS;?></strong></h3>
       <p class="text-center"><a  href="eagle_rocks.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-    </div>
   </div>
   </a>
   </div>
@@ -42,23 +36,19 @@
   <div class="row">
     <a class="link_color" href="swan_village.php">
   <div class="col-md-4">
-    <div class="card">
       <img src="assets/img/swan_village.jpg" class="img-responsive" alt=<?= SWAN_VILLAGE;?>>
       <div class="card-block">
         <h3 class="card-title"><strong><?= SWAN_VILLAGE;?></strong></h3>
         <p class="text-center"><a href="swan_village.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
       </div>
-      </div>
       </a>
     </div>
     <div class="col-md-4">
       <a class="link_color" href="forest.php">
-      <div class="card">
         <img src="assets/img/main3.jpg" class="img-responsive" alt=<?= FOREST;?>>
         <div class="card-block">
           <h3 class="card-title"><strong><?= FOREST;?></strong></h3>
           <p class="text-center"><a href="forest.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-        </div>
         </div>
         </a>
       </div>
