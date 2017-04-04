d10285a3fd127a5d073282afaeef8e791802555e
stefanstankov <stef.stankov@gmail.com>
2017-04-04 15:57:36 +0300

    msg

--- historical.php
+++ historical.php
@@ -4,29 +4,30 @@
    	<?php	require 'includes/header.php';?>
 		<div class="text-center container">
           <h2><?= HISTORICAL;?></h2>
-
+          <div class="row">
+          <div class="col-md-7">
+            <a class="link_color" href="ustra.php">
+            <img src="assets/img/ustra.jpg" class="img-responsive" alt="<?= USTRA;?>">
+              </a>
+        </div>
         <div class="col-md-5">
      <a  class="link_color" href="ustra.php">
     <h3><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h3/>
   <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
          </a>
    </div>
-     <div class="col-md-7">
-       <a class="link_color" href="ustra.php">
-       <img src="assets/img/ustra.jpg" class="img-responsive" alt="<?= USTRA;?>">
-         </a>
-   </div>
- </div>
-  <hr class="featurette-divider">
-  <div class="col-md-7">
-    <a class="link_color" href="perperikon.php">
-    <img src="assets/img/perperikon.jpg" class="img-responsive" alt="<?= PERPERIKON;?>">
-      </a>
 </div>
+  <hr class="featurette-divider">
+  <div class="row">
   <div class="col-md-5">
      <a  class="link_color" href="perperikon.php">
     <h3><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h3/>
   <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
+         </a>
+   </div>
+     <div class="col-md-7">
+       <a class="link_color" href="perperikon.php">
+       <img src="assets/img/perperikon.jpg" class="img-responsive" alt="<?= PERPERIKON;?>">
          </a>
    </div>
  </div>
