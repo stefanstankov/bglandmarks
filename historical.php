9eaf0ad0e2ae7e7d707ca0fb3afce39620fdbf61
stefanstankov <stef.stankov@gmail.com>
2017-04-04 16:29:29 +0300

    msg

--- historical.php
+++ historical.php
@@ -5,31 +5,35 @@
 		<div class="text-center container">
           <h2><?= HISTORICAL;?></h2>
           <div class="row">
-          <div class="col-md-5">
-            <a class="link_color" href="ustra.php">
-            <img src="assets/img/ustra.jpg" class="img-responsive" alt="<?= USTRA;?>">
-              </a>
+    <div class="col-md-7 pull-left">
+      <a class="link_color" href="perperikon.php">
+      <img src="assets/img/perperikon.jpg" class="img-responsive" alt="Перперикон">
+      <div class="card-block">
+        <h3 class="card-title"><strong><?= PERPERIKON;?></strong></h3>
+        <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
         </div>
-        <div class="col-md-5 custom-text">
-     <a  class="link_color" href="ustra.php">
-    <h4><strong><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><strong/><h4/>
-  <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-         </a>
-   </div>
+        </a>
+  </div>
+    <div class="col-md-5">
+
+  </div>
 </div>
-  <hr class="featurette-divider">
-  <div class="row">
-  <div class="col-md-5 custom-text">
-     <a  class="link_color" href="perperikon.php">
-    <h4><strong><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><strong>/<h4/>
-  <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-         </a>
-   </div>
-     <div class="col-md-5">
-       <a class="link_color" href="perperikon.php">
-       <img src="assets/img/perperikon.jpg" class="img-responsive" alt="<?= PERPERIKON;?>">
-         </a>
-   </div>
- </div>
+
+   <hr class="featurette-divider">
+
+
+ <div class="row">
+  <div class="col-md-5">
+  </div>
+    <div class="col-md-7 pull-right">
+      <a class="link_color" href="perperikon.php">
+      <img src="assets/img/perperikon.jpg" class="img-responsive" alt="Перперикон">
+      <div class="card-block">
+        <h3 class="card-title"><strong><?= PERPERIKON;?></strong></h3>
+        <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
+        </div>
+        </a>
+  </div>
+</div>
       </div>
         	<?php require 'includes/footer.php' ?>
