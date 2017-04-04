02c3a7347947c9c5a185d05b69ac7aa37b966f5f
stefanstankov <stef.stankov@gmail.com>
2017-04-04 16:04:28 +0300

    msg

--- historical.php
+++ historical.php
@@ -12,7 +12,7 @@
         </div>
         <div class="col-md-5">
      <a  class="link_color" href="ustra.php">
-    <h3><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h3/>
+    <h4><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h4/>
   <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
          </a>
    </div>
@@ -21,7 +21,7 @@
   <div class="row">
   <div class="col-md-5">
      <a  class="link_color" href="perperikon.php">
-    <h3><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h3/>
+    <h4><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h4/>
   <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
          </a>
    </div>
