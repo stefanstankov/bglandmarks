ea4abc89881908c56d23537cb2748eadaf3ff1ee
stefanstankov <stef.stankov@gmail.com>
2017-04-04 14:37:27 +0300

    msg

--- arhitecture.php
+++ arhitecture.php
@@ -2,57 +2,6 @@
   	$pageTitle = 'Начало';
 ?>
 <?php	require 'includes/header.php';?>
-<div class="jumbotron">
-  <div class="container">
-  <h1 class="custom-div bounceInDown"><?= WELCOME;?></h1>
-<form action="" method="post">
-<input type="text" id="searchBar" name="search" placeholder="Search for landmarks..."/><input type="submit" id="searchBtn" value="Go!" />
-</form>
-
-<div id="demo" class="collapse">
-<br>
-<br>
-<table class="table table-striped table-responsive">
-<thead>
-<tr>
-<th>#</th>
-<th>Natural</th>
-<th>Arhitectural</th>
-<th>Historical</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<th scope="row">1</th>
-<td><a href="utroba_cave.php"><?= UTROBA;?></a></td>
-<td><a href="museum.php"><?= MUSEUM;?></a></td>
-<td><a href="ustra.php"><?= USTRA;?></a></td>
-</tr>
-<tr>
-<th scope="row">2</th>
-<td><a href="stone_mushrooms.php"><?= STONE;?></a></td>
-<td><a href="statue.php"><?= STATUE;?></a></td>
-<td><a href="perperikon.php"><?= PERPERIKON;?></a></td>
-</tr>
-<tr>
-<th scope="row">3</th>
-<td><a href="eagle_rocks.php"><?= EAGLE_ROCKS;?></a></td>
-<td><a href="dam_wall.php"><?= Kardzhali_Dam;?></a></td>
-</tr>
-<tr>
-<th scope="row">4</th>
-<td><a href="swan_village.php"><?= SWAN_VILLAGE;?></a></td>
-<td><a href="devil_bridge.php"><?= BRIDGE;?></a></td>
-</tr>
-<tr>
-<th scope="row">5</th>
-<td><a href="forest.php"><?= FOREST;?></a></a></td>
-</tr>
-</tbody>
-</table>
-</div>
-</div>
-</div>
 		<div class="text-center container">
       <h2><?= ARCHITECTURAL;?></h2>
         <div class="row">
