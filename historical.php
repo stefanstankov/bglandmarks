64b28b7cc564943e411a2507c0c79bb537fa6ccc
stefanstankov <stef.stankov@gmail.com>
2017-04-04 15:55:52 +0300

    msg

--- historical.php
+++ historical.php
@@ -18,15 +18,15 @@
    </div>
  </div>
   <hr class="featurette-divider">
+  <div class="col-md-7">
+    <a class="link_color" href="perperikon.php">
+    <img src="assets/img/perperikon.jpg" class="img-responsive" alt="<?= PERPERIKON;?>">
+      </a>
+</div>
   <div class="col-md-5">
      <a  class="link_color" href="perperikon.php">
     <h3><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span><h3/>
   <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-         </a>
-   </div>
-     <div class="col-md-7">
-       <a class="link_color" href="perperikon.php">
-       <img src="assets/img/perperikon.jpg" class="img-responsive" alt="<?= PERPERIKON;?>">
          </a>
    </div>
  </div>
