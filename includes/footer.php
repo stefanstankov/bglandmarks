0d914bc44dada0254ed97e582cd1b57605750077
stefanstankov <stef.stankov@gmail.com>
2017-04-05 00:47:52 +0300

    msg

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,13 @@
 <footer class="footer footer-color">
   <div class="cunteiner-fluid footer-color">
   <div class="container ">
-      <ul class="footer-ul list-unstyled text-left">
+      <ul class="footer-ul-1 list-unstyled text-left">
       <li><strong><h2><?= CONTACTS;?></h2></strong></li>
       <li><i class="fa fa-map-pin"></i><?= COUNTRY;?></li>
       <li><i class="fa fa-phone"></i> <?= PHONE;?>: +359 899 001 504</li>
     <li><i class="fa fa-envelope"></i><?= MAIL;?>: bglandmarks@gmail.com</li>
     </ul>
-    <ul class="footer-ul list-unstyled text-left">
+    <ul class="footer-ul-2 list-unstyled text-left">
     <li><strong><h2>Follow</h2></strong></li>
     <li><i class="fa fa-instagram"></i><a href="https://www.instagram.com/bglandmarks"><?= INSTAGRAM;?></a></li>
     <li>&copy; <strong class="co">Bulgarian Landmarks</strong></li>
