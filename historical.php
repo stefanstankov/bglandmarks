21ad79dec80fb34dae63dc1242bdc90d256ca063
stefanstankov <stef.stankov@gmail.com>
2017-04-04 15:20:26 +0300

    msg

--- historical.php
+++ historical.php
@@ -5,21 +5,23 @@
 		<div class="text-center container">
           <h2><?= HISTORICAL;?></h2>
           <div class="row featurette">
+                  <a class="link_color" href="perperikon.php">
                   <div class="col-md-7">
-                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading"><span class="text-muted">Perperikon Archeological Complex / Археологически комплекс Перперикон</span></h2>
+                    <p class="lead"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
                   </div>
                   <div class="col-md-5">
                     <img class="featurette-image img-responsive center-block img-style" src="assets/img/perperikon.jpg" alt="<?= PERPERIKON;?>">
                   </div>
+                  <a/>
                 </div>
 
                 <hr class="featurette-divider">
 
                 <div class="row featurette">
                   <div class="col-md-7 col-md-push-5">
-                    <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    <h2 class="featurette-heading"><span class="text-muted">Ustra Medieval Fortress / Средновековна крепост Устра</span></h2>
+                    <p class="lead"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
                   </div>
                   <div class="col-md-5 col-md-pull-7">
                     <img class="featurette-image img-responsive center-block img-style" src="assets/img/ustra.jpg" alt="<?= USTRA;?>">
