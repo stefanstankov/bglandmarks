88aca0a06add3d239048223cf68b52dd9e9b452e
stefanstankov <stef.stankov@gmail.com>
2017-04-04 14:53:26 +0300

    msg

--- historical.php
+++ historical.php
@@ -10,7 +10,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                   </div>
                   <div class="col-md-5">
-                    <img class="featurette-image img-responsive center-block" data-src="assets/img/perperikon.jpg" alt="<?= PERPERIKON;?>">
+                    <img class="featurette-image img-responsive center-block" src="assets/img/perperikon.jpg" alt="<?= PERPERIKON;?>">
                   </div>
                 </div>
 
@@ -22,7 +22,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                   </div>
                   <div class="col-md-5 col-md-pull-7">
-                    <img class="featurette-image img-responsive center-block" data-src="assets/img/ustra.jpg" alt="<?= USTRA;?>">
+                    <img class="featurette-image img-responsive center-block" src="assets/img/ustra.jpg" alt="<?= USTRA;?>">
                   </div>
                 </div>
       </div>
