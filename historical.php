7824834b661fb9f14d1b52deb2e260f7ede2b72b
stefanstankov <stef.stankov@gmail.com>
2017-04-04 14:49:26 +0300

    msg

--- historical.php
+++ historical.php
@@ -4,25 +4,26 @@
    	<?php	require 'includes/header.php';?>
 		<div class="text-center container">
           <h2><?= HISTORICAL;?></h2>
-              <div class="row">
-        <div class="col-md-4">
-          <a  class="link_color" href="ustra.php">
-            <img src="assets/img/ustra.jpg" class="img-responsive" alt="Крепостта Устра">
-            <div class="card-block">
-              <h3 class="card-title"><strong><?= USTRA;?></strong></h3>
-              <p class="text-center"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-            </div>
-              </a>
-        </div>
-    	    <div class="col-md-4">
-            <a class="link_color" href="perperikon.php">
-            <img src="assets/img/perperikon.jpg" class="img-responsive" alt="Перперикон">
-            <div class="card-block">
-              <h3 class="card-title"><strong><?= PERPERIKON;?></strong></h3>
-              <p class="text-center"><a href="perperikon.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-              </div>
-              </a>
-        </div>
-			</div>
+          <div class="row featurette">
+                  <div class="col-md-7">
+                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                  </div>
+                  <div class="col-md-5">
+                    <img class="featurette-image img-responsive center-block" data-src="assets/img/perperikon.jpg" alt="<?= PERPERIKON;?>">
+                  </div>
+                </div>
+
+                <hr class="featurette-divider">
+
+                <div class="row featurette">
+                  <div class="col-md-7 col-md-push-5">
+                    <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                  </div>
+                  <div class="col-md-5 col-md-pull-7">
+                    <img class="featurette-image img-responsive center-block" data-src="assets/img/ustra.jpg" alt="<?= USTRA;?>">
+                  </div>
+                </div>
       </div>
         	<?php require 'includes/footer.php' ?>
