39b0aba63a84e9ad2009520173f84d87a1aad8e3
stefanstankov <stef.stankov@gmail.com>
2017-04-12 19:41:09 +0300

    msg

--- dam_wall.php
+++ dam_wall.php
@@ -69,6 +69,8 @@ ini_set('display_errors', 1);
 <div class="row">
 <div class="col-md-12">
 <div class="fb-comments" data-href="http://www.bglandmarks.com/dam_wall.php" data-numposts="5"></div>
+</div>
+<div class="col-md-12">
 <div class="fb-share-button pull-left" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
 </div>
 </div>
