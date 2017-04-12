c7a4986a7244cabe65a32001485d59bce45a6f8d
stefanstankov <stef.stankov@gmail.com>
2017-04-12 18:13:37 +0300

    msg

--- dam_wall.php
+++ dam_wall.php
@@ -4,22 +4,20 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 	$pageTitle = 'Начало';
+	require 'includes/header.php'
 ?>
-<?php require 'includes/header.php' ?>
+
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" type="text/css" href="/assets/css/app.css" media="screen" />
-
+	<h2 class="text-center><?= Kardzhali_Dam;?></h2>
+		<hr/>
 		<div class="text-center container">
-
-					<h2><?= Kardzhali_Dam;?></h2>
-			      <hr/>
-						<div class="container">
 						<p>
  <a class="fancybox" href="/assets/img/dam_wall.jpg" data-fancybox-group="gallery" title="<?= BRIDGE;?>"><img src="/assets/img/dam_wall.jpg" alt="" /></a>
  <a class="fancybox" href="/assets/img/dam_wall1.jpg" data-fancybox-group="gallery" title="<?= BRIDGE;?>"><img src="/assets/img/dam_wall1.jpg" alt="" /></a>
  <a class="fancybox" href="/assets/img/dam_wall2.jpg" data-fancybox-group="gallery" title="<?= BRIDGE;?>"><img src="/assets/img/dam_wall2.jpg" alt="" /></a>
 </p>
-</div>
+
             <div class="row">
 					<div class="col-md-12">
 						<div class="panel">
