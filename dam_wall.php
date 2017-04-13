60a87b2cbd114ed9d6a61ef5e0c70eda91604894
stefanstankov <stef.stankov@gmail.com>
2017-04-13 11:21:17 +0300

    msg

--- dam_wall.php
+++ dam_wall.php
@@ -57,7 +57,7 @@ ini_set('display_errors', 1);
 										<td><a href="https://en.wikipedia.org/wiki/Kardzhali_Reservoir"><i class="fa fa-wikipedia-w" aria-hidden="true"></i><?= Kardzhali_Dam;?></a></td>
 									</tr>
 									<tr>
-										<td><a href="http://socbg.com/2015/04/%D1%8F%D0%B7%D0%BE%D0%B2%D0%B8%D1%80-%D0%BA%D1%8A%D1%80%D0%B4%D0%B6%D0%B0%D0%BB%D0%B8-%D0%BD%D0%B0%D0%B9-%D0%B3%D0%BE%D0%BB%D1%8F%D0%BC%D0%B0%D1%82%D0%B0-%D0%B2%D0%BE%D0%B4.html">История на язовирната стена</a></td>
+										<td><a href="http://socbg.com/2015/04/%D1%8F%D0%B7%D0%BE%D0%B2%D0%B8%D1%80-%D0%BA%D1%8A%D1%80%D0%B4%D0%B6%D0%B0%D0%BB%D0%B8-%D0%BD%D0%B0%D0%B9-%D0%B3%D0%BE%D0%BB%D1%8F%D0%BC%D0%B0%D1%82%D0%B0-%D0%B2%D0%BE%D0%B4.html"><?= Kardzhali_Dam;?></a></td>
 								</tr>
 								<tr>
 									<td><a href="https://www.youtube.com/embed/z8al6m-BATg"><i class="fa fa-youtube" aria-hidden="true"></i><?= Kardzhali_Dam;?></a></td>
