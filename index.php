6b5d0b78e7a067860599145c6537751c9f6a70b0
stefanstankov <stef.stankov@gmail.com>
2017-04-13 19:55:46 +0300

    msg

--- index.php
+++ index.php
@@ -24,41 +24,67 @@
             </div>
                 </div>
                 </div>
-                <h2 class="text-center text-color1"><?= EDITORS_CHOISE;?></h2>
-<div class="container-fluid text-center cf-custom cf-custom-colorddd">
-   <div class="container">
-<div class="row">
-<div class="col-md-3">
-  <a class="link_color" href="utroba_cave.php">
-         <img src="assets/img/utroba_cave.jpg" class="img-responsive" alt="<?= UTROBA;?>">
-         <h5><span class="text-color"><?= UTROBA;?></span></h5>
+                <!-- Container (Portfolio Section) -->
+<div id="portfolio" class="container-fluid text-center bg-grey">
+<h2 class="text-center text-color1"><?= EDITORS_CHOISE;?></h2>br>
+  <div class="row text-center slideanim">
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="assets/img/utroba_cave.jpg" alt="Paris" width="400" height="300">
+       <h5><span class="text-color"><?= UTROBA;?></span></h5>
          <p class="text-center text-color"><a href="utroba_cave.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-     </a>
-</div>
-<div class="col-md-3">
-  <a class="link_color" href="dam_wall.php">
-          <img src="assets/img/dam_wall.jpg" class="img-responsive" alt="<?= Kardzhali_Dam;?>" >
-          <h5><span class="text-color"> <?= Kardzhali_Dam;?></span></h5>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="ssets/img/dam_wall.jpg" alt="New York" width="400" height="300">
+        <h5><span class="text-color"> <?= Kardzhali_Dam;?></span></h5>
           <p class="text-center text-color"><a href="dam_wall.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-        </a>
-</div>
-<div class="col-md-3">
-  <a  class="link_color" href="ustra.php">
-    <img src="assets/img/ustra.jpg" class="img-responsive" alt="<?= USTRA;?>">
-        <h5> <span class="text-color"><?= USTRA;?></span></h5>
-        <p class="text-center text-color"><a href="ustra.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </a>
-</div>
-<div class="col-md-3">
-  <a  class="link_color" href="stone_mushrooms.php">
-<img src="assets/img/stone_mushrooms.jpg" class="img-responsive" alt="<?= STONE;?>">
+      </div>
+    </div>
+	 <div class="col-sm-3">
+      <div class="thumbnail">
+        <img src="assets/img/stone_mushrooms.jpg" alt="San Francisco" width="400" height="300">
         <h5> <span class="text-color"><?= STONE;?></span></h5>
         <p class="text-center text-color"><a href="stone_mushrooms.php" class="btn btn-primary" role="button"><?= READ_MORE;?></a></p>
-      </a>
+      </div>
+    </div>
+  </div><br>
+
+  <h2>What our customers say</h2>
+  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <h4>"This company is the best. I am so happy with the result!"<br><span>Michael Roe, Vice President, Comment Box</span></h4>
+      </div>
+      <div class="item">
+        <h4>"One word... WOW!!"<br><span>John Doe, Salesman, Rep Inc</span></h4>
+      </div>
+      <div class="item">
+        <h4>"Could I... BE any more happy with this company?"<br><span>Chandler Bing, Actor, FriendsAlot</span></h4>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
-</div>
-</div>
-</div>
+
            <h2 class="text-center text-color1"><?= INSTA;?></h2>
            <div class="container .cf-custom">
          <div class="row">
@@ -111,4 +137,41 @@
        });
 
        </script>
+
+       <script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
             	<?php require 'includes/footer.php' ?>
