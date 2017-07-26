68a1e73daa49d828d9c0f1b08e307fbe44ba68aa
stefanstankov <stef.stankov@gmail.com>
2017-07-26 19:44:47 +0300

    adding dbcon file

--- databaseconnection.php
+++ databaseconnection.php
@@ -7,4 +7,17 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
-echo $hostname.' '.$username.' '.$database;
+$link = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
